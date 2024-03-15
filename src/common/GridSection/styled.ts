52ef0c1779c081196faddb1f3a87a5950c86aa10
traczoskar <155188539+traczoskar@users.noreply.github.com>
2024-03-15 16:18:01 +0100

    added media query and changed gap value

--- src/common/GridSection/styled.ts
+++ src/common/GridSection/styled.ts
@@ -3,5 +3,11 @@ import styled from "styled-components";
 export const GridSection = styled.div`
   display: grid;
   grid-template-columns: repeat(2, 1fr);
-  gap: 1rem;
+  gap: 3.5rem;
+  align-items: center;
+
+  @media (max-width: 768px) {
+    grid-template-columns: 1fr;
+    gap: 2rem;
+  }
 `;
