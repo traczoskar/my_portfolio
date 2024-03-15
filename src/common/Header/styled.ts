71386fdaa7f0e51f80b6d3ad52da885cae4fdfa7
traczoskar <155188539+traczoskar@users.noreply.github.com>
2024-03-15 16:19:19 +0100

    changed styling in Header component

--- src/common/Header/styled.ts
+++ src/common/Header/styled.ts
@@ -5,14 +5,20 @@ export const HeaderWrapper = styled.header`
   justify-content: center;
   align-items: center;
   flex-direction: column;
-  margin-top: 30vh;
+`;
+
+export const HeaderSectionWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: 5vw 0;
 `;
 
 export const HeaderContent = styled.h2`
   font-weight: 700;
   margin: 0;
   padding: 0;
-  font-size: 3.5vw;
+  font-size: 3vw;
 `;
 
 export const SpecialText = styled.span`
@@ -20,8 +26,16 @@ export const SpecialText = styled.span`
 `;
 
 export const SubHeaderContent = styled.h3`
-  margin-top: 1rem;
-  font-size: 2vw;
-  font-weight: 300;
+  margin-top: 0.5rem;
+  font-size: 1.2vw;
+  font-weight: 400;
   letter-spacing: 0.03rem;
+`;
+
+export const Paragraph = styled.p`
+  margin-top: 1rem;
+  font-size: 1.1rem;
+  font-weight: 400;
+  letter-spacing: 0.03rem;
+  line-height: 1.8;
 `;
