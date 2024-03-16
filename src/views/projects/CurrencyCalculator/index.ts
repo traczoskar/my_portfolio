1ee33f3856b6b6442951246aafeadae317fee6d8
traczoskar <155188539+traczoskar@users.noreply.github.com>
2024-03-16 22:59:44 +0100

    created initial files for CurrencyCalculator project page

---
+++ src/views/projects/CurrencyCalculator/index.ts
@@ -0,0 +1,3 @@
+const CurrencyCalculator = () => {};
+
+export default CurrencyCalculator;
---
+++ src/views/projects/CurrencyCalculator/styled.ts
@@ -0,0 +1,6 @@
+import styled from "styled-components";
+
+export const CurrencyCalculatorWrapper = styled.div`
+  margin: 0 auto;
+  padding: 0;
+`;
