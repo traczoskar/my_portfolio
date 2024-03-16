b7889dc0d601eda24a93b5b1c700f5c0556a8dd2
traczoskar <155188539+traczoskar@users.noreply.github.com>
2024-03-16 22:49:20 +0100

    created default initial ContactPage

---
+++ src/views/ContactPage/index.tsx
@@ -0,0 +1,14 @@
+import { MainContainer } from "../../common/Main/styled";
+import { ContactPageWrapper } from "./styled";
+
+const ContactPage = () => {
+  return (
+    <MainContainer>
+      <ContactPageWrapper>
+        <h2>Contact Page</h2>
+      </ContactPageWrapper>
+    </MainContainer>
+  );
+};
+
+export default ContactPage;
