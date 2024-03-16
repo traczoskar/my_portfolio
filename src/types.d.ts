7deb93d72d20ce55f0db45e39d0b5e15f924f5a3
traczoskar <155188539+traczoskar@users.noreply.github.com>
2024-03-16 22:45:50 +0100

    added type module for svg files

--- src/types.d.ts
+++ src/types.d.ts
@@ -2,3 +2,13 @@ declare module "*.png" {
   const value: any;
   export = value;
 }
+
+declare module "*.svg" {
+  import * as React from "react";
+
+  const ReactComponent: React.FunctionComponent<
+    React.SVGProps<SVGSVGElement> & { title?: string }
+  >;
+
+  export const ReactComponent;
+}
