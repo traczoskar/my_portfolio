167f338114772b7fc9f1d1a75708f642fdad2a17
traczoskar <155188539+traczoskar@users.noreply.github.com>
2024-03-16 22:51:57 +0100

    created PortfolioCards window and applied it into Header section

---
+++ src/views/HomePage/Header/PortfolioCards/index.tsx
@@ -0,0 +1,57 @@
+import { useState } from "react";
+import { projects } from "./portfolioList";
+import { motion, AnimatePresence } from "framer-motion";
+import {
+  CardsContainer,
+  Nav,
+  ProjectContainer,
+  ProjectImage,
+  ProjectList,
+  ProjectTab,
+  UnderlineSpan,
+} from "./styled";
+import "./underline.css";
+
+export default function PortfolioCards() {
+  const [selectedTab, setSelectedTab] = useState(projects[0]);
+
+  return (
+    <CardsContainer>
+      <Nav>
+        <ProjectList>
+          {projects.map((project) => (
+            <ProjectTab
+              key={project.label}
+              className={project === selectedTab ? "selected" : ""}
+              onClick={() => setSelectedTab(project)}
+            >
+              {`${project.label}`}
+              {project === selectedTab ? (
+                <UnderlineSpan>
+                  <motion.div className="underline" layoutId="underline" />
+                </UnderlineSpan>
+              ) : null}
+            </ProjectTab>
+          ))}
+        </ProjectList>
+      </Nav>
+      <ProjectContainer>
+        <AnimatePresence mode="wait">
+          <motion.div
+            key={selectedTab ? selectedTab.label : "empty"}
+            initial={{ y: 10, opacity: 0 }}
+            animate={{ y: 0, opacity: 1 }}
+            exit={{ y: -10, opacity: 0 }}
+            transition={{ duration: 0.2 }}
+          >
+            {selectedTab ? (
+              <ProjectImage src={selectedTab.image} alt={selectedTab.label} />
+            ) : (
+              "😋"
+            )}
+          </motion.div>
+        </AnimatePresence>
+      </ProjectContainer>
+    </CardsContainer>
+  );
+}
