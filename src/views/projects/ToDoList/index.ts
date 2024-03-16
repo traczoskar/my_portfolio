70656a1a1e5e33119cf43910078bc28dc8cdade9
traczoskar <155188539+traczoskar@users.noreply.github.com>
2024-03-16 23:00:12 +0100

    created initial files for ToDoList project page

---
+++ src/views/projects/ToDoList/index.ts
@@ -0,0 +1,3 @@
+const ToDoList = () => {};
+
+export default ToDoList;
---
+++ src/views/projects/ToDoList/styled.ts
@@ -0,0 +1,6 @@
+import styled from "styled-components";
+
+export const ToDoListWrapper = styled.div`
+  margin: 0 auto;
+  padding: 0;
+`;
