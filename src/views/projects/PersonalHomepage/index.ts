5484de2ee8b1d77cba14feb31d955aabe364a4c7
traczoskar <155188539+traczoskar@users.noreply.github.com>
2024-03-18 15:52:01 +0100

    added default initial render for PersonalHomepage and changed file type for .tsx

--- src/views/projects/PersonalHomepage/index.ts
+++
@@ -1,3 +0,0 @@
-const PersonalHomepage = () => {};
-
-export default PersonalHomepage;
---
+++ src/views/projects/PersonalHomepage/index.tsx
@@ -0,0 +1,9 @@
+const PersonalHomepage = () => {
+  return (
+    <div>
+      <h1>Personal Homepage</h1>
+    </div>
+  );
+};
+
+export default PersonalHomepage;
