c668b919b4670f4841b53145e7afa265ab55e4b5
traczoskar <155188539+traczoskar@users.noreply.github.com>
2024-03-18 15:51:51 +0100

    added default initial render for MoviesBrowser and changed file type for .tsx

--- src/views/projects/MoviesBrowser/index.ts
+++
@@ -1,3 +0,0 @@
-const MoviesBrowser = () => {};
-
-export default MoviesBrowser;
---
+++ src/views/projects/MoviesBrowser/index.tsx
@@ -0,0 +1,9 @@
+const MoviesBrowser = () => {
+  return (
+    <div>
+      <h1>Movies Browser</h1>
+    </div>
+  );
+};
+
+export default MoviesBrowser;
