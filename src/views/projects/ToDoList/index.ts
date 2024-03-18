acc2a7cb23711527962d2452bc896d102b928161
traczoskar <155188539+traczoskar@users.noreply.github.com>
2024-03-18 15:52:08 +0100

    added default initial render for To Do List and changed file type for .tsx

--- src/views/projects/ToDoList/index.ts
+++
@@ -1,3 +0,0 @@
-const ToDoList = () => {};
-
-export default ToDoList;
---
+++ src/views/projects/ToDoList/index.tsx
@@ -0,0 +1,9 @@
+const ToDoList = () => {
+  return (
+    <div>
+      <h1>ToDoList</h1>
+    </div>
+  );
+};
+
+export default ToDoList;
