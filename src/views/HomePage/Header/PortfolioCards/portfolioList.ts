5ec63f0a9d07d3118195bd14caab0a9ee6dfb0ae
traczoskar <155188539+traczoskar@users.noreply.github.com>
2024-05-04 19:17:52 +0200

    moved Project type to types folder

--- src/views/HomePage/Header/PortfolioCards/portfolioList.ts
+++ src/views/HomePage/Header/PortfolioCards/portfolioList.ts
@@ -2,28 +2,23 @@ import moviesBrowser from "../../../../assets/portfolio_moviesBrowser.png";
 import taskList from "../../../../assets/portfolio_taskList.png";
 import currencyConverter from "../../../../assets/portfolio_currencyConverter.png";
 import personalHomepage from "../../../../assets/portfolio_personalHomepage.png";
+import { Project } from "../../../../types/types";
 
-export interface Projects {
-  label: string;
-  image: string;
-  route: string;
-}
-
-export const projects: Projects[] = [
+export const projects: Project[] = [
   {
     label: "Personal Homepage",
     image: personalHomepage,
-    route: "/projects/personal-homepage",
+    route: "personal-homepage",
   },
   {
     label: "Movies Browser",
     image: moviesBrowser,
-    route: "/projects/movies-browser",
+    route: "movies-browser",
   },
-  { label: "To Do List", image: taskList, route: "/projects/to-do-list" },
+  { label: "To Do List", image: taskList, route: "to-do-list" },
   {
     label: "Currency Calculator",
     image: currencyConverter,
-    route: "/projects/currency-calculator",
+    route: "currency-calculator",
   },
 ];
