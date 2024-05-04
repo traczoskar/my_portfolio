2d3aa4a57b141729fcf6c75c1aa083ade780bff6
traczoskar <155188539+traczoskar@users.noreply.github.com>
2024-05-04 18:00:15 +0200

    changed  initial index file (entry point) for .tsx

--- src/index.tsx
+++ src/index.tsx
@@ -5,7 +5,7 @@ import reportWebVitals from "./reportWebVitals.js";
 import store from "./app/store";
 import { Provider } from "react-redux";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById("root")!);
 
 root.render(
   <React.StrictMode>
