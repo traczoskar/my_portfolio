5bd7fddc42a7a83c48d7c4170f49bc52644ca4f2
traczoskar <155188539+traczoskar@users.noreply.github.com>
2024-05-04 23:39:02 +0200

    developed ProjectDetails component

--- src/views/projects/ProjectDetails/index.tsx
+++ src/views/projects/ProjectDetails/index.tsx
@@ -1,8 +1,30 @@
+import { useNavigate, useParams } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import Modal from "../../../common/Modal";
+import { projects } from "../../HomePage/Header/PortfolioCards/portfolioList";
+import {
+  closeProjectDetails,
+  selectProjectDetailsState,
+} from "../../../slices/projectDetailsSlice";
+import { Project } from "../../../types/types";
+
 const ProjectDetails = () => {
+  const { id } = useParams();
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const { isOpen } = useSelector(selectProjectDetailsState);
+  const project: Project | undefined = projects.find((p) => p.route === id);
+
+  const handleClose = () => {
+    dispatch(closeProjectDetails());
+    navigate(-1);
+  };
+
   return (
-    <div>
-      <h1>Project Details</h1>
-    </div>
+    <Modal isOpen={isOpen} onClose={handleClose}>
+      <h2>{project?.label}</h2>
+      <p>{project?.route}</p>
+    </Modal>
   );
 };
 
