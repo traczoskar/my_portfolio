c7c33a43739392304da4065ee4a6aed45782577a
traczoskar <155188539+traczoskar@users.noreply.github.com>
2024-05-05 16:10:53 +0200

    created PortfolioDescription component

---
+++ src/views/HomePage/Portfolio/PortfolioDescription/index.tsx
@@ -0,0 +1,12 @@
+import { DescriptionWrapper } from "./styled";
+
+const PortfolioDescription = () => {
+  return (
+    <DescriptionWrapper>
+      <h1>Portfolio Description</h1>
+      <p>Click in the project tab to see the details!</p>
+    </DescriptionWrapper>
+  );
+};
+
+export default PortfolioDescription;
