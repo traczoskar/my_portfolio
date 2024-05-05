e409cbb8e1bc3dd8325ce3926faab810e193b631
traczoskar <155188539+traczoskar@users.noreply.github.com>
2024-05-05 16:52:19 +0200

    created styling file for Modal

---
+++ src/common/Modal/styled.ts
@@ -0,0 +1,42 @@
+import styled from "styled-components";
+
+export const ModalOverlay = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.5);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  z-index: 999;
+  backdrop-filter: blur(5px);
+`;
+
+export const ModalContent = styled.div`
+  background-color: white;
+  border-radius: 5px;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.5);
+  max-width: 80%;
+  max-height: 80%;
+  overflow: auto;
+`;
+
+export const CloseButton = styled.button`
+  position: absolute;
+  width: 2.5rem;
+  height: 2.5rem;
+  top: 1rem;
+  right: 1rem;
+  font-size: 1rem;
+  background-color: #dddddd;
+  border: none;
+  border-radius: 0.5rem;
+  cursor: pointer;
+  transition: 0.2s ease;
+
+  &:hover {
+    background-color: #bbbbbb;
+  }
+`;
