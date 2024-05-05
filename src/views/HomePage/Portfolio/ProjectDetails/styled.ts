679e2057067930b5634760bd310d4667360c563f
traczoskar <155188539+traczoskar@users.noreply.github.com>
2024-05-05 16:53:11 +0200

    added styling to ProjectDetails

---
+++ src/views/HomePage/Portfolio/ProjectDetails/styled.ts
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const ProjectDetailsContainer = styled.article`
+  display: flex;
+  flex-direction: column;
+`;
+
+export const ProjectHeader = styled.h2`
+  margin: 2rem 0 0 0;
+  padding: 0 2rem 2rem 2rem;
+  font-size: 2rem;
+  font-weight: 700;
+  border-bottom: 1px solid #ddd;
+`;
