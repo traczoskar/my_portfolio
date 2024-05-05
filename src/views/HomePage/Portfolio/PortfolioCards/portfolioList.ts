daf5f745c61cc4888be07c1e10df427ec6004b9c
traczoskar <155188539+traczoskar@users.noreply.github.com>
2024-05-05 16:52:41 +0200

    developed Projects content

--- src/views/HomePage/Portfolio/PortfolioCards/portfolioList.ts
+++ src/views/HomePage/Portfolio/PortfolioCards/portfolioList.ts
@@ -8,29 +8,77 @@ import { Project } from "../../../../types/types";
 
 export const projects: Project[] = [
   {
+    route: "personal-homepage",
     label: "Personal Homepage",
     image: personalHomepage,
-    route: "personal-homepage",
+    icon: "🏠",
+    tech: ["React", "TypeScript", "Styled Components"],
+    repo: "https://github.com/traczoskar/personal_homepage",
+    live: "https://traczoskar.github.io/personal_homepage/",
+    description:
+      "🗒 As a title suggest it's my personal homepage, a virtual CV👨🏻‍💻 You can find here information about me, showcase of my programming skills, what I want to learn, GitHub repositories and contact section.",
+    features: [
+      "About Me 🙋‍♂️: Interactive section featuring a profile photo and a brief introduction.",
+      "Theme Switch 🌗: You can choose between using dark or light mode up to your preferences.",
+      "Skillset 💪: Detailed list of my current tech skills.",
+      " Learning Goals 🚀: A forward-looking section about skills I'm actively learning.",
+      "GitHub Repositories 📁: Dynamic tiles populated with real-time data from my GitHub repositories using the GitHub API.",
+      " Contact Section 📩: Footer section contains my social media links and my e-mail address.",
+    ],
   },
   {
+    route: "ai-weather-app",
     label: "AI - Weather App",
     image: aiWeatherApp,
-    route: "ai-weather-app",
+    icon: "🌦️",
+    tech: ["React", "TypeScript", "Styled Components"],
+    repo: "https://github.com/traczoskar/weatherwise_ai",
+    live: "https://traczoskar.github.io/weatherwise_ai/",
+    description:
+      "🌦️ AI - Weather App is a simple weather application that uses OpenWeatherMap API to fetch weather data for a given city. The app uses a pre-trained model from TensorFlow.js to predict the weather based on the current temperature.",
   },
   {
+    route: "portfolio-page",
     label: "Portfolio Page",
     image: portfolioPage,
-    route: "portfolio-page",
+    icon: "📁",
+    tech: ["React", "TypeScript", "Styled Components"],
+    repo: "https://github.com/traczoskar/portfolio_traczoskar",
+    live: "https://traczoskar.dev",
+    description:
+      "📁 Portfolio Page is a showcase of my projects. It's a single-page application that allows you to view detailed information about each project, including a description, features, and technologies used.",
   },
   {
+    route: "movies-browser",
     label: "Movies Browser",
     image: moviesBrowser,
-    route: "movies-browser",
+    icon: "🎬",
+    tech: ["React", "TypeScript", "Styled Components"],
+    repo: "https://github.com/traczoskar/movies-browser",
+    live: "https://olgacybulska.github.io/movies-browser/",
+    description:
+      "🎬 Movies Browser is a simple application that allows you to search for movies and view detailed information about them. The app uses The Movie Database API to fetch movie data.",
   },
-  { label: "To Do List", image: taskList, route: "to-do-list" },
   {
+    route: "to-do-list",
+    label: "To Do List",
+    image: taskList,
+    icon: "📝",
+    tech: ["React", "TypeScript", "Styled Components"],
+    repo: "https://github.com/traczoskar/to_do_list_react",
+    live: "https://olgacybulska.github.io/to_do_list_react/",
+    description:
+      "📝 To Do List is a simple task management application that allows you to add, remove, and mark tasks as completed.",
+  },
+  {
+    route: "currency-calculator",
     label: "Currency Calculator",
     image: currencyConverter,
-    route: "currency-calculator",
+    icon: "💵",
+    tech: ["React", "TypeScript", "Styled Components"],
+    repo: "https://github.com/traczoskar/currency_calculator_react",
+    live: "https://olgacybulska.github.io/currency_calculator_react/",
+    description:
+      "💵 Currency Calculator is a simple application that allows you to convert currencies using the latest exchange rates fetched from the NBP API.",
   },
 ];
