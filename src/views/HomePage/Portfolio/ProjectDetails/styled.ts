1869afc0a9bb16768d7cc7cf95f44f39a476e186
traczoskar <155188539+traczoskar@users.noreply.github.com>
2024-05-05 17:21:26 +0200

    added styling for ProjectDetails

--- src/views/HomePage/Portfolio/ProjectDetails/styled.ts
+++ src/views/HomePage/Portfolio/ProjectDetails/styled.ts
@@ -12,3 +12,37 @@ export const ProjectHeader = styled.h2`
   font-weight: 700;
   border-bottom: 1px solid #ddd;
 `;
+
+export const ProjectDescriptionTitle = styled.h3`
+  margin: 0;
+  padding: 2rem 0 0 2rem;
+  font-size: 1.5rem;
+  font-weight: 600;
+`;
+
+export const ProjectDescription = styled.p`
+  margin: 0;
+  padding: 2rem;
+  font-size: 1.3rem;
+  font-weight: 400;
+  line-height: 1.5;
+  border-bottom: 1px solid #ddd;
+`;
+
+export const ProjectFeatureList = styled.ul`
+  padding: 2rem 2rem 2rem 3rem;
+  margin: 0;
+  list-style-type: disc;
+`;
+
+export const ProjectFeature = styled.li`
+  margin: 0;
+  padding: 0.25rem 0;
+  font-size: 1.3rem;
+  font-weight: 400;
+  line-height: 1.5;
+`;
+
+export const ProjectFeatureName = styled.span`
+  font-weight: 600;
+`;
