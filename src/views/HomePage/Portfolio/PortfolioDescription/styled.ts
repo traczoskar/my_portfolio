f5bd8078248f79b53162dd3fad1091149fbcc51c
traczoskar <155188539+traczoskar@users.noreply.github.com>
2024-05-06 15:50:55 +0200

    added styling for PortfolioDescription column

--- src/views/HomePage/Portfolio/PortfolioDescription/styled.ts
+++ src/views/HomePage/Portfolio/PortfolioDescription/styled.ts
@@ -1,7 +1,25 @@
 import styled from "styled-components";
 
+export const PortfolioTitle = styled.h2`
+  margin: 0;
+  padding: 2rem 0;
+  font-size: 2.5rem;
+  font-weight: 700;
+  border-bottom: 1px solid #ddd;
+`;
+
+export const PortfolioDescriptionContent = styled.p`
+  text-align: center;
+  margin: 0;
+  padding: 2rem 0;
+  font-size: 1.2rem;
+  color: #555555;
+  font-weight: 400;
+  line-height: 1.5;
+`;
+
 export const DescriptionWrapper = styled.div`
   display: flex;
   flex-direction: column;
-  justify-content: center;
+  align-items: center;
 `;
