40f87e8c4a935c0dcb44432f6e237097c5645196
traczoskar <155188539+traczoskar@users.noreply.github.com>
2024-05-06 15:49:44 +0200

    developed ClickHere component

--- src/views/HomePage/Portfolio/PortfolioCards/ClickHere/index.tsx
+++ src/views/HomePage/Portfolio/PortfolioCards/ClickHere/index.tsx
@@ -1,11 +1,24 @@
 import { AnimatedDiv, Button } from "./styled";
 import { ReactComponent as ClickArrow } from "../../../../../assets/click.svg";
+import ProjectLink from "../ProjectLink";
+import { useDispatch, useSelector } from "react-redux";
+import { selectSelectedProject } from "../../../../../slices/selectedProjectSlice";
+import { useNavigate } from "react-router";
+import { openProjectDetails } from "../../../../../slices/projectDetailsSlice";
 
 const ClickHere = () => {
+  const selectedProject = useSelector(selectSelectedProject);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const handleLinkClick = () => {
+    dispatch(openProjectDetails(selectedProject.route));
+    navigate(`/projects/${selectedProject.route}`);
+  };
   return (
-    <Button>
+    <Button onClick={handleLinkClick}>
       <AnimatedDiv>
-        <ClickArrow width={15} height={15} />
+        <ClickArrow width={22} height={22} />
       </AnimatedDiv>
       Click here for more details!
     </Button>
