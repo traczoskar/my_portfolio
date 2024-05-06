971cf092bb0f4b0ac3794c94d61ffdbbaaa6b9da
traczoskar <155188539+traczoskar@users.noreply.github.com>
2024-05-06 15:48:29 +0200

    added new slice to Redux store

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import themeReducer from "../slices/themeSlice";
 import projectDetailsReducer from "../slices/projectDetailsSlice";
+import selectedProjectReducer from "../slices/selectedProjectSlice";
 
 const store = configureStore({
   reducer: {
     theme: themeReducer,
     projectDetails: projectDetailsReducer,
+    selectedProject: selectedProjectReducer,
   },
 });
 
