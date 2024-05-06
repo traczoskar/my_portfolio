6aebfd2d3258a7b2ea8cc201f0b9c00fd332fc4f
traczoskar <155188539+traczoskar@users.noreply.github.com>
2024-05-06 10:11:29 +0200

    added styling for technologies

--- src/views/HomePage/Portfolio/ProjectDetails/styled.ts
+++ src/views/HomePage/Portfolio/ProjectDetails/styled.ts
@@ -23,7 +23,7 @@ export const ProjectDescriptionTitle = styled.h3`
 export const ProjectDescription = styled.p`
   margin: 0;
   padding: 2rem;
-  font-size: 1.3rem;
+  font-size: 1.2rem;
   font-weight: 400;
   line-height: 1.5;
   border-bottom: 1px solid #ddd;
@@ -33,16 +33,49 @@ export const ProjectFeatureList = styled.ul`
   padding: 2rem 2rem 2rem 3rem;
   margin: 0;
   list-style-type: disc;
+  border-bottom: 1px solid #ddd;
 `;
 
 export const ProjectFeature = styled.li`
   margin: 0;
   padding: 0.25rem 0;
-  font-size: 1.3rem;
+  font-size: 1.2rem;
   font-weight: 400;
   line-height: 1.5;
 `;
 
 export const ProjectFeatureName = styled.span`
   font-weight: 600;
+`;
+
+export const TechnologiesWrapper = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  gap: 2rem;
+  padding: 2rem 1rem 2rem 2rem;
+`;
+
+export const TechnologyContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 0.5rem;
+  justify-content: center;
+`;
+
+export const TechnologyIcon = styled.img`
+  width: auto;
+  height: 50px;
+  filter: grayscale(100%);
+
+  &:hover {
+    filter: grayscale(0%);
+  }
+`;
+
+export const TechnologySubtitle = styled.h4`
+  margin: 0;
+  padding: 0;
+  font-weight: 500;
+  font-size: 0.8rem;
 `;
