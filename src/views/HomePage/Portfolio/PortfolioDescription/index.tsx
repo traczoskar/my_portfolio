b9f09c7e2f99d355fcf07807473ae52f380ccf2e
traczoskar <155188539+traczoskar@users.noreply.github.com>
2024-05-06 15:50:41 +0200

    developed Portfolio section description column

--- src/views/HomePage/Portfolio/PortfolioDescription/index.tsx
+++ src/views/HomePage/Portfolio/PortfolioDescription/index.tsx
@@ -1,10 +1,21 @@
-import { DescriptionWrapper } from "./styled";
+import ClickHere from "../PortfolioCards/ClickHere";
+import {
+  DescriptionWrapper,
+  PortfolioTitle,
+  PortfolioDescriptionContent,
+} from "./styled";
 
 const PortfolioDescription = () => {
   return (
     <DescriptionWrapper>
-      <h1>Portfolio Description</h1>
-      <p>Click in the project tab to see the details!</p>
+      <PortfolioTitle>My recent projects</PortfolioTitle>
+      <PortfolioDescriptionContent>
+        Here's a curated collection of my recent projects. Click on any project
+        to delve into more details about the technologies utilized and the
+        skills acquired through their development journey. Take your time to
+        browse and learn more about my work!
+      </PortfolioDescriptionContent>
+      <ClickHere />
     </DescriptionWrapper>
   );
 };
