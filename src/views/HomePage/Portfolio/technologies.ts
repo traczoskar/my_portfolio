653664f345b089164a223b0635fe84b51504e676
traczoskar <155188539+traczoskar@users.noreply.github.com>
2024-05-06 10:10:04 +0200

    created technologies file to store tech stack data

---
+++ src/views/HomePage/Portfolio/technologies.ts
@@ -0,0 +1,88 @@
+import type_script from "../../../assets/technologies/type_script.png";
+import java_script from "../../../assets/technologies/java_script.png";
+import html5 from "../../../assets/technologies/html.png";
+import css3 from "../../../assets/technologies/css.png";
+import react from "../../../assets/technologies/react.png";
+import redux from "../../../assets/technologies/redux.png";
+import redux_saga from "../../../assets/technologies/redux_saga.png";
+import styled_components from "../../../assets/technologies/styled_components.png";
+import react_router from "../../../assets/technologies/react_router.png";
+import figma from "../../../assets/technologies/figma.png";
+import axios from "../../../assets/technologies/axios.png";
+import github from "../../../assets/technologies/github.png";
+import npm from "../../../assets/technologies/npm.png";
+import openai from "../../../assets/technologies/openai.png";
+import tailwind from "../../../assets/technologies/tailwind.png";
+import vite from "../../../assets/technologies/vite.png";
+import { Technology } from "../../../types/types";
+
+export const technologies: Technology[] = [
+  {
+    name: "TypeScript",
+    icon: type_script,
+  },
+  {
+    name: "JavaScript ES6+",
+    icon: java_script,
+  },
+  {
+    name: "HTML5",
+    icon: html5,
+  },
+  {
+    name: "CSS3",
+    icon: css3,
+  },
+  {
+    name: "React",
+    icon: react,
+  },
+  {
+    name: "Redux",
+    icon: redux,
+  },
+  {
+    name: "Redux Saga",
+    icon: redux_saga,
+  },
+  {
+    name: "Styled Components",
+    icon: styled_components,
+  },
+  {
+    name: "React Router",
+    icon: react_router,
+  },
+  {
+    name: "Figma",
+    icon: figma,
+  },
+  {
+    name: "Axios",
+    icon: axios,
+  },
+  {
+    name: "GitHub",
+    icon: github,
+  },
+  {
+    name: "GitHub API",
+    icon: github,
+  },
+  {
+    name: "NPM",
+    icon: npm,
+  },
+  {
+    name: "OpenAI",
+    icon: openai,
+  },
+  {
+    name: "Tailwind CSS",
+    icon: tailwind,
+  },
+  {
+    name: "Vite",
+    icon: vite,
+  },
+];
