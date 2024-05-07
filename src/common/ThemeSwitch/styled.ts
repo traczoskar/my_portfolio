f9600b693c7980cc50c75247f7e21055ae8f5422
traczoskar <155188539+traczoskar@users.noreply.github.com>
2024-05-07 22:12:35 +0200

    updated styling of ThemeSwitch

--- src/common/ThemeSwitch/styled.ts
+++ src/common/ThemeSwitch/styled.ts
@@ -1,15 +1,23 @@
 import styled, { css } from "styled-components";
-import { ReactComponent as ThemeSwitchIcon } from "../../assets/themeSwitch.svg";
 
 export const Button = styled.button`
   background: none;
   display: flex;
   align-items: center;
   color: inherit;
+  border-radius: 0.25rem;
+
   cursor: pointer;
   border: none;
-  padding: 0;
-  margin-left: 1vw;
+  padding: 0.5rem 1rem;
+
+  &:hover {
+    background: #cccccc;
+    border: 1px solid #eeeeee;
+  }
+  &:active {
+    transform: scale(0.9);
+  }
 `;
 
 interface IconWrapperProps {
@@ -17,25 +25,15 @@ interface IconWrapperProps {
 }
 
 export const IconWrapper = styled.div<IconWrapperProps>`
-  background-color: ${({ theme }) => theme.colors.themeSwitch.background};
-  width: 35px;
-  height: 35px;
-  border: 2px solid ${({ theme }) => theme.colors.themeSwitch.border};
-  padding: 3px;
-  border-radius: 50%;
+  background: inherit;
   display: flex;
-  box-shadow: 0 1px 1px hsl(0deg 0% 0% / 0.075),
-    0 2px 2px hsl(0deg 0% 0% / 0.075), 0 4px 4px hsl(0deg 0% 0% / 0.075),
-    0 8px 8px hsl(0deg 0% 0% / 0.075), 0 16px 16px hsl(0deg 0% 0% / 0.075);
-  transition: transform ease-in-out 0.5s;
-
-  ${({ $changeposition }) =>
-    $changeposition &&
-    css`
-      transform: rotateY(360deg);
-    `}
-`;
-
-export const Icon = styled(ThemeSwitchIcon)`
   color: ${({ theme }) => theme.colors.themeSwitch.button};
+  svg {
+    ${({ $changeposition }) =>
+      $changeposition &&
+      css`
+        transform: rotateY(180deg);
+      `}
+    transition: ease-in-out 0.3s;
+  }
 `;
