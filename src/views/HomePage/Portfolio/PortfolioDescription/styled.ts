6d4738f22e482bd22f60d05d77aba98a42ce1302
traczoskar <155188539+traczoskar@users.noreply.github.com>
2024-05-07 11:11:21 +0200

    changed PortfolioDescription styling

--- src/views/HomePage/Portfolio/PortfolioDescription/styled.ts
+++ src/views/HomePage/Portfolio/PortfolioDescription/styled.ts
@@ -2,16 +2,17 @@ import styled from "styled-components";
 
 export const PortfolioTitle = styled.h2`
   margin: 0;
-  padding: 2rem 0;
+  padding: 0 0 3rem 0;
   font-size: 2.5rem;
   font-weight: 700;
   border-bottom: 1px solid #ddd;
+  text-shadow: -4px 6px 8px rgba(66, 68, 90, 0.2);
 `;
 
 export const PortfolioDescriptionContent = styled.p`
   text-align: center;
   margin: 0;
-  padding: 2rem 0;
+  padding: 3rem 0;
   font-size: 1.2rem;
   color: #555555;
   font-weight: 400;
