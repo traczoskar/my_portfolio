a960a2c5ae83a8bcdb098fead96540420d9dc0b5
traczoskar <155188539+traczoskar@users.noreply.github.com>
2024-05-09 22:01:20 +0200

    created initial aboutMeData file to store about me info

---
+++ src/views/AboutMe/aboutMeData.ts
@@ -0,0 +1,35 @@
+import avatar from "../../assets/avatar.jpg";
+
+export const ABOUT = {
+  name: "Oskar",
+  surname: "Tracz",
+  title: "Frontend Developer",
+  description:
+    "I am a Frontend Developer with a passion for creating web applications. I am a self-taught developer who is always eager to learn new technologies and improve my skills. I am a team player who is always ready to help and share knowledge with others. I am looking for new opportunities to grow and develop my skills.",
+  avatar: avatar,
+  techStack: [
+    "React",
+    "TypeScript",
+    "JavaScript ES6+",
+    "HTML5",
+    "CSS3",
+    "Styled Components",
+    "Redux",
+    "Redux Saga",
+    "Axios",
+    "Figma",
+    "GitHub",
+    "NPM",
+  ],
+  email: "traczoskar@gmail.com",
+  socials: [
+    {
+      name: "GitHub",
+      url: "https://github.com/traczoskar",
+    },
+    {
+      name: "LinkedIn",
+      url: "https://www.linkedin.com/in/traczoskar/",
+    },
+  ],
+};
