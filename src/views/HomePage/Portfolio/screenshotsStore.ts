922aeb473328fc76ade525f99b036127102702f0
traczoskar <155188539+traczoskar@users.noreply.github.com>
2024-05-09 22:05:32 +0200

    created screenshotsStore file for screenshots of projects

---
+++ src/views/HomePage/Portfolio/screenshotsStore.ts
@@ -0,0 +1,24 @@
+import personalHomepage_1 from "../../../assets/portfolio/personalHomepage/personalHomepage_1.png";
+import personalHomepage_2 from "../../../assets/portfolio/personalHomepage/personalHomepage_2.png";
+import personalHomepage_3 from "../../../assets/portfolio/personalHomepage/personalHomepage_3.png";
+import personalHomepage_4 from "../../../assets/portfolio/personalHomepage/personalHomepage_4.png";
+import { Screenshot } from "../../../types/types";
+
+export const personalHomepageScreenshots: Screenshot[] = [
+  {
+    alt: "Personal Homepage",
+    imageUrl: personalHomepage_1,
+  },
+  {
+    alt: "Personal Homepage",
+    imageUrl: personalHomepage_2,
+  },
+  {
+    alt: "Personal Homepage",
+    imageUrl: personalHomepage_3,
+  },
+  {
+    alt: "Personal Homepage",
+    imageUrl: personalHomepage_4,
+  },
+];
