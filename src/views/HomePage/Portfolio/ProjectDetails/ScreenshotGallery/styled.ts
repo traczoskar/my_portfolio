eb498fedd5d94a091be062598dcc909ec955883e
traczoskar <155188539+traczoskar@users.noreply.github.com>
2024-05-09 22:06:54 +0200

    changed minor styling details in ScreenshotGallery

--- src/views/HomePage/Portfolio/ProjectDetails/ScreenshotGallery/styled.ts
+++ src/views/HomePage/Portfolio/ProjectDetails/ScreenshotGallery/styled.ts
@@ -2,12 +2,24 @@ import styled from "styled-components";
 
 export const GalleryContainer = styled.div`
   display: grid;
-  grid-template-columns: repeat(3, 1fr);
-  gap: 1rem;
+  grid-template-columns: repeat(4, 1fr);
+  gap: 1.5rem;
+  padding: 2rem 0;
 `;
 
 export const Thumbnail = styled.img`
   width: 100%;
   height: auto;
+  border-radius: 0.5rem;
   cursor: pointer;
+  box-shadow: 0 1px 1px hsl(0deg 0% 0% / 0.075),
+    0 2px 2px hsl(0deg 0% 0% / 0.075), 0 4px 4px hsl(0deg 0% 0% / 0.075),
+    0 8px 8px hsl(0deg 0% 0% / 0.075), 0 16px 16px hsl(0deg 0% 0% / 0.075);
+  transition: 0.2s;
+
+  &:hover {
+    opacity: 0.9;
+    transform: scale(1.05);
+    outline: orange solid 5px;
+  }
 `;
