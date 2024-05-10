872af79792947a6f0a5a09c9ccdd2c5de0d33d77
traczoskar <155188539+traczoskar@users.noreply.github.com>
2024-05-10 22:15:00 +0200

    added Footer to HomePage view

--- src/views/HomePage/index.tsx
+++ src/views/HomePage/index.tsx
@@ -3,6 +3,7 @@ import { MainContainer } from "../../common/Main/styled";
 import Advantages from "./Advantages";
 import Portfolio from "./Portfolio";
 import MoreAboutMe from "./MoreAboutMe";
+import Footer from "../../common/Footer";
 
 const HomePage = () => {
   return (
@@ -11,7 +12,8 @@ const HomePage = () => {
         <Header title="Frontend" subtitle="Craft. Refine. Optimize." />
         <Portfolio />
         <Advantages />
-        <MoreAboutMe />
+        {/* <MoreAboutMe /> */}
+        <Footer />
       </MainContainer>
     </>
   );
