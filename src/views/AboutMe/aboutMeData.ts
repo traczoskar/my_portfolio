1c72c3d9c4c1032ea2de375d99aaac1847e4ceb1
traczoskar <155188539+traczoskar@users.noreply.github.com>
2024-05-10 18:01:25 +0200

    updated aboutMeData file

--- src/views/AboutMe/aboutMeData.ts
+++ src/views/AboutMe/aboutMeData.ts
@@ -21,6 +21,29 @@ export const ABOUT = {
     "GitHub",
     "NPM",
   ],
+  education: [
+    {
+      name: "Frontend Developer",
+      organization: "YouCode.pl",
+      date: "2024",
+      content:
+        "The course covered the basics of HTML, CSS, JavaScript, React, and Redux. I learned how to create responsive websites and web applications. I also learned how to use Git and GitHub to manage my projects. I completed the course with a final 5-week group project that showcased my skills and trained my teamwork abilities.",
+    },
+    {
+      name: "TypeScript",
+      organization: "YouCode.pl",
+      date: "2024",
+      content:
+        "The course covered the knowledge of TypeScript and how to use it with React. I learned how to create type-safe applications and how to use TypeScript to improve my code quality. It contained topics like types, interfaces, generics, and advanced TypeScript features.",
+    },
+    {
+      name: "AI_devs2 #Reloaded",
+      organization: "AI_devs",
+      date: "2024",
+      content:
+        "The course covered the basics of AI and how to connect it with web applications. I learned how to connect Generative AI with application logic and automatization tools. I completed the course by solving many tasks and creating a personal AI assistant that can help me in multiple ways. I also learned how to use OpenAI API and how to decide when to use AI in my projects. We trained prompt engineering and how to use AI in a responsible way. We also skilled techniques like function calling, building dynamic context and prompts, using vector bases like Qdrant/Pinecone for multiple ways and fine-tuning of models.",
+    },
+  ],
   email: "traczoskar@gmail.com",
   socials: [
     {
