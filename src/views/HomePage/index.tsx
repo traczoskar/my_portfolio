ad0d64dd100e88ac4de42f71bc533e97aa644997
traczoskar <155188539+traczoskar@users.noreply.github.com>
2024-05-10 18:00:59 +0200

    added MoreAboutMe component import to HomePage view

--- src/views/HomePage/index.tsx
+++ src/views/HomePage/index.tsx
@@ -2,15 +2,16 @@ import Header from "./Header";
 import { MainContainer } from "../../common/Main/styled";
 import Advantages from "./Advantages";
 import Portfolio from "./Portfolio";
+import MoreAboutMe from "./MoreAboutMe";
 
 const HomePage = () => {
   return (
     <>
       <MainContainer>
         <Header title="Frontend" subtitle="Craft. Refine. Optimize." />
-
         <Portfolio />
         <Advantages />
+        <MoreAboutMe />
       </MainContainer>
     </>
   );
