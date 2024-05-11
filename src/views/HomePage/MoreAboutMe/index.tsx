6b84b40f7a07ffb31192f6ea76baf013878991ef
traczoskar <155188539+traczoskar@users.noreply.github.com>
2024-05-11 13:52:21 +0200

    developed MoreAboutMe component

--- src/views/HomePage/MoreAboutMe/index.tsx
+++ src/views/HomePage/MoreAboutMe/index.tsx
@@ -1,8 +1,52 @@
+import {
+  AboutMeContainer,
+  AboutMeHeader,
+  AboutMeLink,
+  AboutMeText,
+  MoreAboutMeDivider,
+  MoreAboutMeSection,
+  MoreAboutMeWrapper,
+  Picture,
+  TechStackContainer,
+  Technology,
+  TechnologyIcon,
+  TechnologySubtitle,
+} from "./styled";
+import picture from "../../../assets/avatar.jpg";
+import { ABOUT } from "../../AboutMe/aboutMeData";
+import { technologies } from "../Portfolio/technologies";
+
 const MoreAboutMe = () => {
   return (
-    <div>
-      <h2>More About Me</h2>
-    </div>
+    <MoreAboutMeSection>
+      <MoreAboutMeDivider>
+        <TechStackContainer>
+          {ABOUT?.techStack
+            ? ABOUT.techStack.map((tech, index) => (
+                <Technology>
+                  <TechnologyIcon
+                    key={index}
+                    src={technologies.find((t) => t.name === tech)?.icon}
+                    alt={tech}
+                  />
+                  <TechnologySubtitle>{tech}</TechnologySubtitle>
+                </Technology>
+              ))
+            : null}
+        </TechStackContainer>
+
+        <MoreAboutMeWrapper>
+          <Picture src={picture} alt="Oskar Tracz" />
+          <AboutMeContainer>
+            <AboutMeHeader>More about me</AboutMeHeader>
+            <AboutMeText>Click the button below and check out.</AboutMeText>
+            <AboutMeLink to="/about" title="About Me page">
+              About me
+            </AboutMeLink>
+          </AboutMeContainer>
+        </MoreAboutMeWrapper>
+      </MoreAboutMeDivider>
+    </MoreAboutMeSection>
   );
 };
 
