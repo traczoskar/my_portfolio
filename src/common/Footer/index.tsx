69f979cd7e5e1c4880fb301e956e24e09d6f36a7
traczoskar <155188539+traczoskar@users.noreply.github.com>
2024-05-11 13:53:27 +0200

    added github and LinkedIn icons to footer

--- src/common/Footer/index.tsx
+++ src/common/Footer/index.tsx
@@ -4,8 +4,14 @@ import {
   FooterWrapper,
   HeaderLink,
   LetsChat,
+  SocialLink,
+  Socials,
   StyledWavesSVG,
 } from "./styled";
+import { ReactComponent as ChatIcon } from "../../assets/icons/chat.svg";
+import { ReactComponent as GitHubIcon } from "../../assets/icons/git_icon.svg";
+import { ReactComponent as LinkedIcon } from "../../assets/icons/linkedIn_icon.svg";
+import { ABOUT } from "../../views/AboutMe/aboutMeData";
 
 const Footer = () => {
   return (
@@ -15,8 +21,24 @@ const Footer = () => {
           <PageLogo width={25} height={25} />
           traczoskar.dev
         </HeaderLink>
+        <Socials>
+          {ABOUT.socials.map((social) => (
+            <SocialLink
+              href={social.url}
+              target="_blank"
+              rel="noreferrer noopener"
+            >
+              {social.name === "GitHub" ? (
+                <GitHubIcon width={30} height={30} />
+              ) : social.name === "LinkedIn" ? (
+                <LinkedIcon width={30} height={30} />
+              ) : null}
+            </SocialLink>
+          ))}
+        </Socials>
         <LetsChat to="/contact" title="Contact page">
           Let's chat!
+          <ChatIcon width={23} height={23} />
         </LetsChat>
       </FooterContent>
       <StyledWavesSVG />
