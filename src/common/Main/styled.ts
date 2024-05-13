e0cbe2d83bb68da64795afac0d1860903a7310e9
traczoskar <155188539+traczoskar@users.noreply.github.com>
2024-05-13 16:09:53 +0200

    changed styling of MainContainer component to pass on background-color as a props

--- src/common/Main/styled.ts
+++ src/common/Main/styled.ts
@@ -1,6 +1,12 @@
 import styled from "styled-components";
+import { DefaultTheme } from "styled-components";
 
-export const MainContainer = styled.main`
+interface MainContainerProps {
+  backgroundColor?: (theme: DefaultTheme) => string;
+}
+
+export const MainContainer = styled.main<MainContainerProps>`
   display: flex;
   flex-direction: column;
+  background-color: ${({ backgroundColor }) => backgroundColor || "inherit"};
 `;
