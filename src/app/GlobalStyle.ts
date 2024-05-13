746f328cefb60c3835627a0fd0f003f0f0051ace
traczoskar <155188539+traczoskar@users.noreply.github.com>
2024-05-13 16:07:47 +0200

    updated GlobalStyle.ts

--- src/app/GlobalStyle.ts
+++ src/app/GlobalStyle.ts
@@ -3,6 +3,8 @@ import { createGlobalStyle } from "styled-components";
 export const GlobalStyle = createGlobalStyle`
     html {
         box-sizing: border-box;
+        height: 100vh;
+        margin: 0;
     }
 
     *, ::after, ::before {
@@ -11,6 +13,8 @@ export const GlobalStyle = createGlobalStyle`
 
     body {
         font-family: "Inter", sans-serif;
+        height: 100vh;
+        margin: 0;
         word-break: break-word;
         transition: background ease 1s ;
         transition: color ease 0.5s;
@@ -19,5 +23,11 @@ export const GlobalStyle = createGlobalStyle`
         color: ${({ theme }) => theme.colors.textBasic};
         letter-spacing: 0.03em;
         line-height: 1.3;
+    }
+
+    #root {
+        min-height: 100vh;
+        display: flex;
+        flex-direction: column;
     }
 `;
