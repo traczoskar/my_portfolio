b02962190a11c993ea02005aa3fbfc1938763e37
traczoskar <155188539+traczoskar@users.noreply.github.com>
2024-05-13 16:58:45 +0200

    added MainConainer and Footer with passing props with color

--- src/views/ContactPage/index.tsx
+++ src/views/ContactPage/index.tsx
@@ -10,35 +10,38 @@ import {
 import { ReactComponent as GitHubIcon } from "../../assets/icons/contact/github.svg";
 import { ReactComponent as LinkedInIcon } from "../../assets/icons/contact/linkedin.svg";
 import { ReactComponent as MailIcon } from "../../assets/icons/contact/mail.svg";
-import Footer from "../../common/Footer";
 import NavSection from "../../common/NavSection";
+import Footer from "../../common/Footer";
 
 const ContactPage = () => {
   return (
-    <MainContainer>
-      <NavSection />
-      <ContactPageWrapper>
-        <ContactTitle>
-          Drop me <SpecialText>a line</SpecialText>, I'm just{" "}
-          <SpecialText>a click</SpecialText> away!
-        </ContactTitle>
-        <ContactLinks>
-          <ContactLink href={`mailto:${ABOUT.email}`}>
-            <MailIcon width={30} height={30} />
-            E-mail
-          </ContactLink>
-          <ContactLink href={ABOUT.socials[0].url} target="_blank">
-            <GitHubIcon width={30} height={30} />
-            GitHub
-          </ContactLink>
-          <ContactLink href={ABOUT.socials[1].url} target="_blank">
-            <LinkedInIcon width={30} height={30} />
-            LinkedIn
-          </ContactLink>
-          <ContactLink>contact@traczoskar.dev</ContactLink>
-        </ContactLinks>
-      </ContactPageWrapper>
-    </MainContainer>
+    <>
+      <MainContainer backgroundColor="white">
+        <NavSection />
+        <ContactPageWrapper>
+          <ContactTitle>
+            Drop me <SpecialText>a line</SpecialText>, I'm just{" "}
+            <SpecialText>a click</SpecialText> away!
+          </ContactTitle>
+          <ContactLinks>
+            <ContactLink href={`mailto:${ABOUT.email}`}>
+              <MailIcon width={30} height={30} />
+              E-mail
+            </ContactLink>
+            <ContactLink href={ABOUT.socials[0].url} target="_blank">
+              <GitHubIcon width={30} height={30} />
+              GitHub
+            </ContactLink>
+            <ContactLink href={ABOUT.socials[1].url} target="_blank">
+              <LinkedInIcon width={30} height={30} />
+              LinkedIn
+            </ContactLink>
+            <ContactLink>contact@traczoskar.dev</ContactLink>
+          </ContactLinks>
+        </ContactPageWrapper>
+      </MainContainer>
+      <Footer backgroundColor="white" />
+    </>
   );
 };
 
