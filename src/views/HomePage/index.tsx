3d66edebfa0ca2d6206c20fa8a0ebba604e008cf
traczoskar <155188539+traczoskar@users.noreply.github.com>
2024-05-13 16:11:17 +0200

    added MainContainer backgroundColor props  and NavSection to HomePage view

--- src/views/HomePage/index.tsx
+++ src/views/HomePage/index.tsx
@@ -3,17 +3,19 @@ import { MainContainer } from "../../common/Main/styled";
 import Advantages from "./Advantages";
 import Portfolio from "./Portfolio";
 import MoreAboutMe from "./MoreAboutMe";
-import Footer from "../../common/Footer";
+import NavSection from "../../common/NavSection";
 
 const HomePage = () => {
   return (
     <>
-      <MainContainer>
+      <MainContainer
+        backgroundColor={({ theme }) => theme.colors.app.background}
+      >
+        <NavSection />
         <Header title="Frontend" subtitle="Craft. Refine. Optimize." />
         <Portfolio />
         <Advantages />
         <MoreAboutMe />
-        <Footer />
       </MainContainer>
     </>
   );
