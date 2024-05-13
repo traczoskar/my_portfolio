149ade39e1fc636ddbf11be5301aacfbca8abf88
traczoskar <155188539+traczoskar@users.noreply.github.com>
2024-05-13 16:58:14 +0200

    corrected type declaration for MainContainerProps and added flex-grow: 1; to MainContainer

--- src/common/Main/styled.ts
+++ src/common/Main/styled.ts
@@ -2,11 +2,12 @@ import styled from "styled-components";
 import { DefaultTheme } from "styled-components";
 
 interface MainContainerProps {
-  backgroundColor?: (theme: DefaultTheme) => string;
+  backgroundColor?: ((theme: DefaultTheme) => string) | string;
 }
 
 export const MainContainer = styled.main<MainContainerProps>`
   display: flex;
   flex-direction: column;
+  flex-grow: 1;
   background-color: ${({ backgroundColor }) => backgroundColor || "inherit"};
 `;
