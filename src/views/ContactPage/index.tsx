44b85f05b5da9fa27cb3f0f7687f15c6658afee5
traczoskar <155188539+traczoskar@users.noreply.github.com>
2024-05-14 20:38:43 +0200

    updated ContactPage with ModalMessage conditional render when e-mail address has been copied to clipboard

--- src/views/ContactPage/index.tsx
+++ src/views/ContactPage/index.tsx
@@ -1,5 +1,6 @@
 import { MainContainer } from "../../common/Main/styled";
 import { ABOUT } from "../AboutMe/aboutMeData";
+import { CopyToClipboard } from "react-copy-to-clipboard";
 import {
   ContactLink,
   ContactLinks,
@@ -10,10 +11,23 @@ import {
 import { ReactComponent as GitHubIcon } from "../../assets/icons/contact/github.svg";
 import { ReactComponent as LinkedInIcon } from "../../assets/icons/contact/linkedin.svg";
 import { ReactComponent as MailIcon } from "../../assets/icons/contact/mail.svg";
+import { ReactComponent as CopyIcon } from "../../assets/icons/contact/copy.svg";
 import NavSection from "../../common/NavSection";
 import Footer from "../../common/Footer";
+import { useEffect, useState } from "react";
+import ModalMessage from "./ModalMessage";
 
 const ContactPage = () => {
+  const [isCopied, setIsCopied] = useState<boolean>(false);
+
+  useEffect(() => {
+    if (isCopied) {
+      setTimeout(() => {
+        setIsCopied(false);
+      }, 2500);
+    }
+  }, [isCopied]);
+
   return (
     <>
       <MainContainer backgroundColor="white">
@@ -24,20 +38,40 @@ const ContactPage = () => {
             <SpecialText>a click</SpecialText> away!
           </ContactTitle>
           <ContactLinks>
-            <ContactLink href={`mailto:${ABOUT.email}`}>
+            <ContactLink
+              href={`mailto:${ABOUT.email}`}
+              title="E-mail to contact@traczoskar.dev"
+            >
               <MailIcon width={30} height={30} />
               E-mail
             </ContactLink>
-            <ContactLink href={ABOUT.socials[0].url} target="_blank">
+            <ContactLink
+              href={ABOUT.socials[0].url}
+              target="_blank"
+              title="GitHub profile"
+            >
               <GitHubIcon width={30} height={30} />
               GitHub
             </ContactLink>
-            <ContactLink href={ABOUT.socials[1].url} target="_blank">
+            <ContactLink
+              href={ABOUT.socials[1].url}
+              target="_blank"
+              title="LinkedIn profile"
+            >
               <LinkedInIcon width={30} height={30} />
               LinkedIn
             </ContactLink>
-            <ContactLink>contact@traczoskar.dev</ContactLink>
+            <CopyToClipboard
+              text={ABOUT.email}
+              onCopy={() => setIsCopied(true)}
+            >
+              <ContactLink title="Copy e-mail address">
+                <CopyIcon width={30} height={30} />
+                contact@traczoskar.dev
+              </ContactLink>
+            </CopyToClipboard>
           </ContactLinks>
+          <ModalMessage isCopied={isCopied} />
         </ContactPageWrapper>
       </MainContainer>
       <Footer backgroundColor="white" />
