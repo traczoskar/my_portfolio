2bb5bf695b6a3c26782a308c4abcd8ad7a874e25
traczoskar <155188539+traczoskar@users.noreply.github.com>
2024-05-15 16:43:31 +0200

    deleted pdfjs worker configuration

--- src/index.tsx
+++ src/index.tsx
@@ -4,9 +4,6 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals.js";
 import store from "./app/store";
 import { Provider } from "react-redux";
-import { pdfjs } from "react-pdf";
-
-pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
 const root = ReactDOM.createRoot(document.getElementById("root")!);
 
