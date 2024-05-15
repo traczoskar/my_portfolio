f378089c7d20b8c4bec69873570c0fc5c4209ac8
traczoskar <155188539+traczoskar@users.noreply.github.com>
2024-05-15 13:32:26 +0200

    moved styling to separate file and added Education component import

--- src/views/AboutMe/index.tsx
+++ src/views/AboutMe/index.tsx
@@ -1,78 +1,24 @@
-import React from "react";
-import styled from "styled-components";
 import { ABOUT } from "./aboutMeData";
 import { MainContainer } from "../../common/Main/styled";
 import NavSection from "../../common/NavSection";
 import Footer from "../../common/Footer";
-
-const AboutMeContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  padding: 2rem;
-  background-color: #f5f5f5;
-`;
-
-const Avatar = styled.img`
-  width: 200px;
-  height: 200px;
-  border-radius: 50%;
-  object-fit: cover;
-  margin-bottom: 1rem;
-`;
-
-const Name = styled.h1`
-  font-size: 2rem;
-  margin-bottom: 0.5rem;
-`;
-
-const Title = styled.h2`
-  font-size: 1.5rem;
-  color: #666;
-  margin-bottom: 1rem;
-`;
-
-const Description = styled.p`
-  font-size: 1rem;
-  text-align: center;
-  margin-bottom: 2rem;
-`;
-
-const TechStack = styled.div`
-  display: flex;
-  flex-wrap: wrap;
-  justify-content: center;
-  gap: 0.5rem;
-`;
-
-const TechItem = styled.span`
-  background-color: #333;
-  color: #fff;
-  padding: 0.5rem 1rem;
-  border-radius: 0.5rem;
-  font-size: 0.9rem;
-`;
-
-const SocialLinks = styled.div`
-  display: flex;
-  gap: 1rem;
-  margin-top: 2rem;
-`;
-
-const SocialLink = styled.a`
-  color: #333;
-  font-size: 1.2rem;
-  transition: color 0.3s ease;
-
-  &:hover {
-    color: #666;
-  }
-`;
+import {
+  AboutMeContainer,
+  Avatar,
+  Description,
+  Name,
+  SocialLink,
+  SocialLinks,
+  TechItem,
+  TechStack,
+  Title,
+} from "./styled";
+import Education from "./Education";
 
 const AboutMe: React.FC = () => {
   return (
     <>
-      <MainContainer>
+      <MainContainer backgroundColor="white">
         <NavSection />
         <AboutMeContainer>
           <Avatar src={ABOUT.avatar} alt="Avatar" />
@@ -98,9 +44,10 @@ const AboutMe: React.FC = () => {
               </SocialLink>
             ))}
           </SocialLinks>
+          <Education educationData={ABOUT.education} />
         </AboutMeContainer>
       </MainContainer>
-      <Footer backgroundColor="inherit" />
+      <Footer backgroundColor="white" />
     </>
   );
 };
