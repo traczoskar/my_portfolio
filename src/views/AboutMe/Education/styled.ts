19714fd59594081e044f611f15c73c4022a4a294
traczoskar <155188539+traczoskar@users.noreply.github.com>
2024-05-15 13:33:03 +0200

    added styling for Education component

---
+++ src/views/AboutMe/Education/styled.ts
@@ -0,0 +1,41 @@
+import styled from "styled-components";
+
+export const EducationContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 2rem;
+  background-color: #f5f5f5;
+`;
+
+export const Title = styled.h1`
+  font-size: 2rem;
+  margin-bottom: 1rem;
+`;
+
+export const EducationList = styled.ul`
+  list-style: none;
+  padding: 0;
+`;
+
+export const EducationItem = styled.li`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-bottom: 1rem;
+`;
+
+export const EducationTitle = styled.h2`
+  font-size: 1.5rem;
+  margin-bottom: 0.5rem;
+`;
+
+export const EducationDescription = styled.p`
+  font-size: 1rem;
+  text-align: center;
+`;
+
+export const EducationDate = styled.p`
+  font-size: 0.9rem;
+  color: #666;
+`;
