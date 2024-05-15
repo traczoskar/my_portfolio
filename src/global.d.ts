2bd59b2d05841c7c680d2b983a20ac7244227d83
traczoskar <155188539+traczoskar@users.noreply.github.com>
2024-05-15 13:31:00 +0200

    added PDF files module declaration

--- src/global.d.ts
+++ src/global.d.ts
@@ -1,13 +1,22 @@
+//---PDF declaration---
+declare module "*.pdf" {
+  const content: string;
+  export default content;
+}
+
+//---PNG declaration---
 declare module "*.png" {
   const value: any;
   export = value;
 }
 
+//---JPG declaration---
 declare module "*.jpg" {
   const value: any;
   export = value;
 }
 
+//---SVG declaration---
 declare module "*.svg" {
   import * as React from "react";
 
