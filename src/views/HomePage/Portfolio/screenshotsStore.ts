88f649dbe5b275e28544fa98fbfba9bd1a342e4b
traczoskar <155188539+traczoskar@users.noreply.github.com>
2024-05-16 17:03:44 +0200

    created store of screenshots for movies browser project

--- src/views/HomePage/Portfolio/screenshotsStore.ts
+++ src/views/HomePage/Portfolio/screenshotsStore.ts
@@ -2,6 +2,13 @@ import personalHomepage_1 from "../../../assets/portfolio/personalHomepage/perso
 import personalHomepage_2 from "../../../assets/portfolio/personalHomepage/personalHomepage_2.png";
 import personalHomepage_3 from "../../../assets/portfolio/personalHomepage/personalHomepage_3.png";
 import personalHomepage_4 from "../../../assets/portfolio/personalHomepage/personalHomepage_4.png";
+import moviesBrowser_1 from "../../../assets/portfolio/moviesBrowser/moviesBrowser_1.png";
+import moviesBrowser_2 from "../../../assets/portfolio/moviesBrowser/moviesBrowser_2.png";
+import moviesBrowser_3 from "../../../assets/portfolio/moviesBrowser/moviesBrowser_3.png";
+import moviesBrowser_4 from "../../../assets/portfolio/moviesBrowser/moviesBrowser_4.png";
+import moviesBrowser_5 from "../../../assets/portfolio/moviesBrowser/moviesBrowser_5.png";
+import moviesBrowser_6 from "../../../assets/portfolio/moviesBrowser/moviesBrowser_6.png";
+import moviesBrowser_7 from "../../../assets/portfolio/moviesBrowser/moviesBrowser_7.png";
 import { Screenshot } from "../../../types/types";
 
 export const personalHomepageScreenshots: Screenshot[] = [
@@ -20,5 +27,35 @@ export const personalHomepageScreenshots: Screenshot[] = [
   {
     alt: "Personal Homepage",
     imageUrl: personalHomepage_4,
+  },
+];
+export const moviesBrowserScreenshots: Screenshot[] = [
+  {
+    alt: "Movies Browser",
+    imageUrl: moviesBrowser_1,
+  },
+  {
+    alt: "Movies Browser",
+    imageUrl: moviesBrowser_2,
+  },
+  {
+    alt: "Movies Browser",
+    imageUrl: moviesBrowser_3,
+  },
+  {
+    alt: "Movies Browser",
+    imageUrl: moviesBrowser_4,
+  },
+  {
+    alt: "Movies Browser",
+    imageUrl: moviesBrowser_5,
+  },
+  {
+    alt: "Movies Browser",
+    imageUrl: moviesBrowser_6,
+  },
+  {
+    alt: "Movies Browser",
+    imageUrl: moviesBrowser_7,
   },
 ];
