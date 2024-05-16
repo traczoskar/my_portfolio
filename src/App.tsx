7f1a08fa790922fbf1559a9412a923826c765ec3
traczoskar <155188539+traczoskar@users.noreply.github.com>
2024-05-16 15:42:22 +0200

    moved AnimatedRoutes to separate folder in "app"

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,20 @@
-import NavSection from "./common/NavSection";
-import HomePage from "./views/HomePage";
+import { BrowserRouter as Router } from "react-router-dom";
 import { ThemeProvider } from "styled-components";
 import { GlobalStyle } from "./app/GlobalStyle";
 import { themeDark, themeLight } from "./app/theme";
 import { useSelector } from "react-redux";
 import { selectIsDarkTheme } from "./slices/themeSlice";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import ContactPage from "./views/ContactPage";
-import ProjectDetails from "./views/HomePage/Portfolio/ProjectDetails";
-import AboutMe from "./views/AboutMe";
-import Footer from "./common/Footer";
+import AnimatedRoutes from "./app/AnimatedRoutes/AnimatedRoutes";
 
 const App = () => {
   const isDarkTheme = useSelector(selectIsDarkTheme);
+
   return (
     <ThemeProvider theme={isDarkTheme ? themeDark : themeLight}>
       <GlobalStyle />
-      <>
-        <Router>
-          <Routes>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/about" element={<AboutMe />} />
-            <Route path="/contact" element={<ContactPage />} />
-            <Route path="/projects/:id" element={<ProjectDetails />} />
-            <Route path="*" element={<div>Not Found</div>} />
-          </Routes>
-        </Router>
-      </>
+      <Router>
+        <AnimatedRoutes />
+      </Router>
     </ThemeProvider>
   );
 };
