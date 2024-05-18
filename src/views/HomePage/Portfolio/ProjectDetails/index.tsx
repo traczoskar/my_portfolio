c23f7edc7ecd13d4c22d49458a1fdf1e5a4da8cd
traczoskar <155188539+traczoskar@users.noreply.github.com>
2024-05-18 15:45:53 +0200

    Update project details page with navigation buttons and keyboard instructions

--- src/views/HomePage/Portfolio/ProjectDetails/index.tsx
+++ src/views/HomePage/Portfolio/ProjectDetails/index.tsx
@@ -3,13 +3,17 @@ import { useDispatch, useSelector } from "react-redux";
 import Modal from "../../../../common/Modal";
 import {
   closeProjectDetails,
+  openProjectDetails,
   selectProjectDetailsState,
 } from "../../../../slices/projectDetailsSlice";
 import { Project } from "../../../../types/types";
 import { projects } from "../portfolioList";
 import {
   ButtonsWrapper,
+  HeaderSection,
   LinkButton,
+  NavigationButton,
+  NavigationWrapper,
   ProjectDescription,
   ProjectDetailsContainer,
   ProjectFeature,
@@ -31,28 +35,96 @@ import { technologies } from "../technologies";
 import { ReactComponent as GitHubIcon } from "../../../../assets/icons/git_icon.svg";
 import { ReactComponent as LiveIcon } from "../../../../assets/icons/www.svg";
 import ScreenshotGallery from "./ScreenshotGallery";
+import { useEffect, useRef } from "react";
+import KeyboardInstruct from "./KeyboardInstruct";
 
 const ProjectDetails = () => {
-  const { id } = useParams();
+  const { id } = useParams<{ id: string }>();
   const dispatch = useDispatch();
   const navigate = useNavigate();
   const { isOpen } = useSelector(selectProjectDetailsState);
-  const project: Project | undefined = projects.find((p) => p.route === id);
+  const projectIndex = projects.findIndex((p) => p.route === id);
+  const project: Project | undefined = projects[projectIndex];
+  const containerRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (id) {
+      dispatch(openProjectDetails(id));
+    }
+  }, [id, dispatch]);
+
+  useEffect(() => {
+    const handleKeyDown = (event: KeyboardEvent) => {
+      if (event.key === "Escape") {
+        handleClose();
+      } else if (event.key === "ArrowRight") {
+        handleNextProject();
+      } else if (event.key === "ArrowLeft") {
+        handlePreviousProject();
+      } else if (event.key === "ArrowUp") {
+        window.scrollBy(0, -100);
+      } else if (event.key === "ArrowDown") {
+        window.scrollBy(0, 100);
+      }
+    };
+
+    window.addEventListener("keydown", handleKeyDown);
+
+    return () => {
+      window.removeEventListener("keydown", handleKeyDown);
+    };
+  }, [projectIndex]);
+
+  useEffect(() => {
+    if (containerRef.current) {
+      containerRef.current.focus();
+    }
+  }, [isOpen]);
 
   const handleClose = () => {
     dispatch(closeProjectDetails());
-    navigate(-1);
+    navigate("/#projects");
+  };
+
+  const handleNextProject = () => {
+    if (projectIndex < projects.length - 1) {
+      navigate(`/projects/${projects[projectIndex + 1].route}`);
+    }
+  };
+
+  const handlePreviousProject = () => {
+    if (projectIndex > 0) {
+      navigate(`/projects/${projects[projectIndex - 1].route}`);
+    }
   };
 
   return (
     <Modal isOpen={isOpen} onClose={handleClose}>
-      <ProjectDetailsContainer>
+      <ProjectDetailsContainer ref={containerRef} tabIndex={-1}>
         {
           //---Header---
         }
-        <ProjectHeader>
-          {project?.icon} {project?.label}
-        </ProjectHeader>
+        <HeaderSection>
+          <ProjectHeader>
+            {project?.icon} {project?.label}
+          </ProjectHeader>
+          <KeyboardInstruct />
+          <NavigationWrapper>
+            <NavigationButton
+              onClick={handlePreviousProject}
+              disabled={projectIndex <= 0}
+            >
+              Previous Project
+            </NavigationButton>
+            <NavigationButton
+              onClick={handleNextProject}
+              disabled={projectIndex >= projects.length - 1}
+            >
+              Next Project
+            </NavigationButton>
+          </NavigationWrapper>
+        </HeaderSection>
+
         {
           //---Description---
         }
