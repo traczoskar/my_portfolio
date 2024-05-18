69e7a600e1bf8c7a6d85eac3c22071f49889d7c9
traczoskar <155188539+traczoskar@users.noreply.github.com>
2024-05-19 00:53:24 +0200

    refactor: Improve screenshot display and instructions in ScreenshotGallery component

--- src/views/HomePage/Portfolio/ProjectDetails/ScreenshotGallery/styled.ts
+++ src/views/HomePage/Portfolio/ProjectDetails/ScreenshotGallery/styled.ts
@@ -11,7 +11,7 @@ export const Thumbnail = styled.img`
   width: 100%;
   height: auto;
   border-radius: 0.5rem;
-  cursor: pointer;
+  cursor: zoom-in;
   box-shadow: 0 1px 1px hsl(0deg 0% 0% / 0.075),
     0 2px 2px hsl(0deg 0% 0% / 0.075), 0 4px 4px hsl(0deg 0% 0% / 0.075),
     0 8px 8px hsl(0deg 0% 0% / 0.075), 0 16px 16px hsl(0deg 0% 0% / 0.075);
