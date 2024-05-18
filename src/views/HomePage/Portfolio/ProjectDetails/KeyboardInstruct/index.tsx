a0c11ada34cdeaa8e9d1e2679394d7d408978a1b
traczoskar <155188539+traczoskar@users.noreply.github.com>
2024-05-18 16:48:24 +0200

    refactor: Refactor KeyboardInstruct component and update project details page styling

--- src/views/HomePage/Portfolio/ProjectDetails/KeyboardInstruct/index.tsx
+++ src/views/HomePage/Portfolio/ProjectDetails/KeyboardInstruct/index.tsx
@@ -1,13 +1,5 @@
-import {
-  KeyboardButtons,
-  KeyboardInstructContainer,
-  KeyboardInstructText,
-} from "./styled";
-import { ReactComponent as ArrowUp } from "../../../../../assets/icons/keyboard/arrow-up.svg";
-import { ReactComponent as ArrowRight } from "../../../../../assets/icons/keyboard/arrow-right.svg";
-import { ReactComponent as ArrowDown } from "../../../../../assets/icons/keyboard/arrow-down.svg";
-import { ReactComponent as ArrowLeft } from "../../../../../assets/icons/keyboard/arrow-left.svg";
-import { ReactComponent as Escape } from "../../../../../assets/icons/keyboard/escape.svg";
+import Keyboard from "./Keyboard";
+import { KeyboardInstructContainer, KeyboardInstructText } from "./styled";
 
 const KeyboardInstruct = () => {
   return (
@@ -18,25 +10,7 @@ const KeyboardInstruct = () => {
         <strong>Arrow Right</strong> to change the project. <br />
         Press <strong>Escape</strong> to close the project.
       </KeyboardInstructText>
-      <KeyboardButtons>
-        <div className="arrow up">
-          <ArrowUp />
-        </div>
-        <div className="arrow left">
-          <ArrowLeft />
-        </div>
-        <div className="arrow down">
-          <ArrowDown />
-        </div>
-        <div className="arrow right">
-          <ArrowRight />
-        </div>
-      </KeyboardButtons>
-      <KeyboardButtons>
-        <div className="arrow escape">
-          <Escape />
-        </div>
-      </KeyboardButtons>
+      <Keyboard />
     </KeyboardInstructContainer>
   );
 };
