3705ab093bd1ff113345af0c43365b105216b1d1
traczoskar <155188539+traczoskar@users.noreply.github.com>
2024-05-20 19:22:50 +0200

    refactor: Improve button styling and add hover effect

--- src/views/HomePage/Header/Button/styled.ts
+++ src/views/HomePage/Header/Button/styled.ts
@@ -9,17 +9,21 @@ export const Button = styled(Link)`
   background: ${({ theme }) => theme.colors.button.background};
   border-radius: 30px;
   text-decoration: none;
-  font-weight: 700;
+  font-weight: 600;
   color: ${({ theme }) => theme.colors.button.text};
   box-shadow: 0 1px 1px hsl(0deg 0% 0% / 0.075),
     0 2px 2px hsl(0deg 0% 0% / 0.075), 0 4px 4px hsl(0deg 0% 0% / 0.075);
   transition: 0.3s ease;
 
   &:hover {
-    filter: brightness(115%);
-    span {
-      transition: 0.3s ease-in-out;
-      transform: scale(1.05);
-    }
+    filter: brightness(105%);
+    transform: translateY(-5px);
+    box-shadow: 0 1px 1px ${({ theme }) => theme.colors.basic},
+      0 2px 2px ${({ theme }) => theme.colors.basic};
+  }
+
+  &:active {
+    transform: scale(0.9);
+    box-shadow: none;
   }
 `;
