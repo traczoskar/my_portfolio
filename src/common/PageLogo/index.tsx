9c48fcd9e5f3afd3749ee4f3a4ef1fb1e0356e76
traczoskar <155188539+traczoskar@users.noreply.github.com>
2024-05-20 19:21:49 +0200

    refactor: Remove unused code and optimize PageLogo component

--- src/common/PageLogo/index.tsx
+++ src/common/PageLogo/index.tsx
@@ -1,5 +1,3 @@
-import { useSelector } from "react-redux";
-import { selectIsDarkTheme } from "../../slices/themeSlice";
 import { LogoContainer } from "./styled";
 import Logo from "./SVGLogo";
 
@@ -9,15 +7,9 @@ interface PageLogoProps {
 }
 
 const PageLogo = ({ width, height }: PageLogoProps) => {
-  const isDarkTheme = useSelector(selectIsDarkTheme);
-
   return (
     <LogoContainer>
-      <Logo
-        stroke={isDarkTheme ? "#DDDDDD" : "#222222"}
-        height={height}
-        width={width}
-      />
+      <Logo height={height} width={width} />
     </LogoContainer>
   );
 };
