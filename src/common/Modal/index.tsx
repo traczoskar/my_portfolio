c38a4bb353bd590e2ede1762abc8fd4a92e06378
traczoskar <155188539+traczoskar@users.noreply.github.com>
2024-05-23 15:04:05 +0200

    refactor: Add ThemeSwitch to Modal for theme customization

--- src/common/Modal/index.tsx
+++ src/common/Modal/index.tsx
@@ -1,4 +1,10 @@
-import { CloseButton, ModalContent, ModalOverlay } from "./styled";
+import { ThemeSwitch } from "../ThemeSwitch";
+import {
+  CloseButton,
+  ModalContent,
+  ModalOverlay,
+  ThemeSwitchModalContainer,
+} from "./styled";
 
 interface ModalProps {
   isOpen: boolean;
@@ -12,6 +18,9 @@ const Modal = ({ isOpen, onClose, children }: ModalProps) => {
   return (
     <ModalOverlay>
       <ModalContent>
+        <ThemeSwitchModalContainer>
+          <ThemeSwitch />
+        </ThemeSwitchModalContainer>
         <CloseButton onClick={onClose}>X</CloseButton>
         {children}
       </ModalContent>
