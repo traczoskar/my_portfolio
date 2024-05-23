2aba75aaa6e3add8cc9373dcd3b474c111a72368
traczoskar <155188539+traczoskar@users.noreply.github.com>
2024-05-23 13:43:28 +0200

    refactor: updated theme.ts colors for themes

--- src/app/theme.ts
+++ src/app/theme.ts
@@ -1,34 +1,47 @@
 const colorNames = {
   white: "#ffffff",
-  darkGray: "#222222",
-  gray: "#444444",
-  lightGray: "#d2d2d2",
+  coal: "#000000",
+  darkGray: "#333333",
+  emperorGray: "#555555",
+  dustyGray: "#999999",
+  lightGray: "#DDDDDD",
 
   // ---darkTheme---
-  platinum: "#eaeaeaff",
+  platinum: "#eaeaeaff", //text color
+  bonJour: "#E8E3E8",
   byzantium: "#893168ff",
   violetJtc: "#4a1942ff",
+  eminence: "#552a82",
   darkPurple: "#2e1c2bff",
-  black: "#050404ff",
-  darkGradient: "#121a26",
+  mirage: "#121a26",
+  dullLavender: "#b7a9e4",
+  blueWood: "#797193",
   violet: "#a770ff",
   veniceBlue: "#a17100",
-  corn: "#f2a365",
+  deluge: "#7f5b9d",
+  trendyPink: "#8f75a3",
+  advantagesGradient: "linear-gradient(to top,  #121A26FF, #552A82FF);",
   buttonDarkGradient: "linear-gradient(to right, #9d50bb, #6e48aa)",
-  buttonDarkGradientReversed: "linear-gradient(to left, #9d50bb, #6e48aa)",
-  violetGradient: "linear-gradient(to right, #a770ff, #834d9b)",
+  violetGradient: "linear-gradient(to right, #a770ff, #834d9b)", // header text {Developer}
+  darkGradient:
+    "linear-gradient(90deg, rgba(18,26,38,1) 0%, rgba(47,36,55,1) 100%)", // advantages background
+
   // ---gradients---
-  gradientRedDark: "linear-gradient(to right, #ff512f, #f09819)",
+
   // ---lightTheme---
-  onyx: "#2e3532ff",
+  sunshade: "#FF9E29",
+  onyx: "#2e3532ff", //text color
+  gray: "#444444", //additional text color (advantages)
   leather: "#A17B5E",
+  gallery: "#EEEEEE",
   orange: "#d66802",
+  rajah: "#f9ad66",
   alabaster: "#E3E3E3",
   timberWolf: "#d2d4c8ff",
   teaGreen: "#d3efbdff",
   buttonLightGradient: "linear-gradient(to right, #f12711, #f5af19)",
-  buttonLightGradientReversed: "linear-gradient(to left, #f12711, #f5af19)",
-  orangeGradient: "linear-gradient(to right, #c02425, #f0cb35)",
+  orangeGradient: "linear-gradient(to right, #c02425, #f0cb35)", // // header text {Developer}
+  lightOrangeGradient: "linear-gradient(to right, #ff512f, #f09819)", // advantages background
 } as const;
 
 const common = {
@@ -44,9 +57,10 @@ const common = {
 export const themeLight = {
   ...common,
   colors: {
-    basic: colorNames.orange,
+    basic: colorNames.sunshade,
     textBasic: colorNames.onyx,
     textAdditional: colorNames.leather,
+    textReversed: colorNames.platinum,
     app: {
       background: colorNames.alabaster,
     },
@@ -55,27 +69,36 @@ export const themeLight = {
     },
     button: {
       background: colorNames.buttonLightGradient,
-      backgroundReversed: colorNames.buttonLightGradientReversed,
       text: colorNames.platinum,
     },
     section: {
       primary: colorNames.white,
+      background: colorNames.white,
     },
     advantages: {
       text: colorNames.platinum,
-      textSecondary: colorNames.leather,
-      background: colorNames.gradientRedDark,
+      textSecondary: colorNames.gray,
+      sectionBackground: colorNames.white,
+      background: colorNames.lightOrangeGradient,
     },
     projects: {
       primary: colorNames.lightGray,
       secondary: colorNames.orange,
-      text: colorNames.alabaster,
-      selected: colorNames.darkGray,
+      background: colorNames.white,
+      border: colorNames.gallery,
+      tabBackground: colorNames.rajah,
+      tabBorder: colorNames.dustyGray,
+      textSecondary: colorNames.gray,
+      selectedText: colorNames.darkGray,
+      selectedBackground: colorNames.lightGray,
+      clickHere: {
+        background: colorNames.rajah,
+        text: colorNames.onyx,
+        border: colorNames.gallery,
+      },
     },
     themeSwitch: {
-      background: colorNames.alabaster,
-      button: colorNames.onyx,
-      border: colorNames.orange,
+      fill: colorNames.violet,
     },
   },
 };
@@ -86,34 +109,45 @@ export const themeDark = {
     basic: colorNames.violet,
     textBasic: colorNames.platinum,
     textAdditional: colorNames.lightGray,
+    textReversed: colorNames.onyx,
     app: {
-      background: colorNames.darkGradient,
+      background: colorNames.mirage,
     },
     header: {
       text: colorNames.violetGradient,
     },
     button: {
       background: colorNames.buttonDarkGradient,
-      backgroundReversed: colorNames.buttonDarkGradientReversed,
       text: colorNames.platinum,
     },
     section: {
       primary: colorNames.darkPurple,
+      background: colorNames.eminence,
     },
     advantages: {
       text: colorNames.platinum,
-      background: colorNames.gradientRedDark,
+      textSecondary: colorNames.bonJour,
+      sectionBackground: colorNames.advantagesGradient,
+      background: colorNames.buttonDarkGradient,
     },
     projects: {
-      primary: colorNames.platinum,
+      primary: colorNames.deluge,
       secondary: colorNames.platinum,
-      text: colorNames.black,
-      selected: colorNames.lightGray,
+      border: colorNames.trendyPink,
+      background: colorNames.mirage,
+      tabBackground: colorNames.emperorGray,
+      tabBorder: colorNames.blueWood,
+      textSecondary: colorNames.bonJour,
+      selectedText: colorNames.darkGray,
+      selectedBackground: colorNames.dullLavender,
+      clickHere: {
+        background: colorNames.deluge,
+        text: colorNames.platinum,
+        border: colorNames.trendyPink,
+      },
     },
     themeSwitch: {
-      background: colorNames.darkGray,
-      button: colorNames.platinum,
-      border: colorNames.violet,
+      fill: colorNames.sunshade,
     },
   },
 };
