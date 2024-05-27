cacd08c9648664f12fe601488df3b2fc89db5795
traczoskar <155188539+traczoskar@users.noreply.github.com>
2024-05-27 15:58:05 +0200

    refactor: Update AboutMe component description and structure

--- src/views/AboutMe/index.tsx
+++ src/views/AboutMe/index.tsx
@@ -5,10 +5,7 @@ import Footer from "../../common/Footer";
 import {
   AboutMeContainer,
   Avatar,
-  Description,
   Name,
-  SocialLink,
-  SocialLinks,
   TechIcon,
   TechItem,
   TechStack,
@@ -18,30 +15,67 @@ import {
   Profession,
   TileTitle,
   TileDiv,
-  GridWrapper,
+  Wrapper,
+  DescriptionFirst,
+  DescriptionAdditional,
+  ParagraphContainer,
+  DescriptionTitle,
 } from "./styled";
 import Education from "./Education";
 import { technologies } from "../HomePage/Portfolio/technologies";
+import { useTheme } from "styled-components";
+import AnimatedIcons from "./AnimatedIcons";
+import { useSelector } from "react-redux";
+import { selectIsDarkTheme } from "../../slices/themeSlice";
 
 const AboutMe: React.FC = () => {
+  const isDarkTheme = useSelector(selectIsDarkTheme);
+  const theme = useTheme();
   return (
     <>
-      <MainContainer backgroundColor="white">
+      <MainContainer backgroundColor={theme.colors.aboutMePage.background}>
         <NavSection />
+        <AnimatedIcons isDarkTheme={isDarkTheme} />
         <AboutMeContainer>
-          <GridWrapper>
+          <Wrapper>
             <Tile>
-              <TileTitle>Who Am I? 👨🏻‍💻</TileTitle>
               <TileDiv>
                 <Avatar src={ABOUT.avatar} alt="Avatar" />
                 <TileDivider>
+                  <TileTitle>Who Am I? 👨🏻‍💻</TileTitle>
                   <Name>
                     {ABOUT.name} {ABOUT.surname}
                   </Name>
                   <Profession>{ABOUT.title}</Profession>
-                  <Description>{ABOUT.description}</Description>
+                  <DescriptionFirst>
+                    {ABOUT.description.paragraph1}
+                  </DescriptionFirst>
                 </TileDivider>
               </TileDiv>
+              <ParagraphContainer>
+                <DescriptionTitle color={theme.colors.aboutMePage.title1}>
+                  #passion ❤️‍🔥
+                </DescriptionTitle>
+                <DescriptionAdditional>
+                  {ABOUT.description.paragraph2}
+                </DescriptionAdditional>
+              </ParagraphContainer>
+              <ParagraphContainer>
+                <DescriptionTitle color={theme.colors.aboutMePage.title2}>
+                  #collaboration 🤝
+                </DescriptionTitle>
+                <DescriptionAdditional>
+                  {ABOUT.description.paragraph3}
+                </DescriptionAdditional>
+              </ParagraphContainer>
+              <ParagraphContainer>
+                <DescriptionTitle color={theme.colors.aboutMePage.title3}>
+                  #commitment 🚀
+                </DescriptionTitle>
+                <DescriptionAdditional>
+                  {ABOUT.description.paragraph4}
+                </DescriptionAdditional>
+              </ParagraphContainer>
             </Tile>
             <Tile>
               <TileTitle>My Tech Stack 🛠</TileTitle>
@@ -57,23 +91,11 @@ const AboutMe: React.FC = () => {
                 ))}
               </TechStack>
             </Tile>
-          </GridWrapper>
-          <SocialLinks>
-            {ABOUT.socials.map((social, index) => (
-              <SocialLink
-                key={index}
-                href={social.url}
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                {social.name}
-              </SocialLink>
-            ))}
-          </SocialLinks>
+          </Wrapper>
           <Education />
         </AboutMeContainer>
       </MainContainer>
-      <Footer backgroundColor="white" />
+      <Footer backgroundColor={theme.colors.aboutMePage.footer} />
     </>
   );
 };
