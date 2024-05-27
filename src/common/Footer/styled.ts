64ca0ceb246e83db7988f318bb1254b45cbca3c7
traczoskar <155188539+traczoskar@users.noreply.github.com>
2024-05-27 16:13:03 +0200

    chore: Update Footer component styling for HeaderLink

--- src/common/Footer/styled.ts
+++ src/common/Footer/styled.ts
@@ -26,6 +26,7 @@ export const FooterContent = styled.div`
 
 export const HeaderLink = styled(HashLink)`
   text-decoration: none;
+  color: inherit;
   display: flex;
   align-items: center;
   gap: 1.25rem;
