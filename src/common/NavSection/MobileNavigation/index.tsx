0a3930af00bf7726f30fe18d80d5dbc9045113df
traczoskar <155188539+traczoskar@users.noreply.github.com>
2024-05-28 18:52:18 +0200

    refactor: Add MobileNavigation component for mobile menu feature

---
+++ src/common/NavSection/MobileNavigation/index.tsx
@@ -0,0 +1,76 @@
+import { ThemeSwitch } from "../../ThemeSwitch";
+import {
+  CloseButton,
+  GitHubIcon,
+  LinkedInIcon,
+  MobileHashLink,
+  MobileIconLink,
+  MobileNavLinks,
+  MobileRouterLink,
+} from "./styled";
+
+interface MobileNavigationProps {
+  isOpen: boolean;
+  onClick: () => void;
+}
+
+const MobileNavigation: React.FC<MobileNavigationProps> = ({
+  isOpen,
+  onClick,
+}) => {
+  return (
+    <>
+      <MobileNavLinks isOpen={isOpen}>
+        <CloseButton onClick={onClick}>X</CloseButton>
+        <MobileRouterLink
+          title="About Me"
+          aria-label="Link to About Me page"
+          to="/about"
+          onClick={onClick}
+        >
+          About Me
+        </MobileRouterLink>
+        <MobileHashLink
+          smooth
+          to="/#projects"
+          aria-label="Link to Projects section"
+          title="Projects"
+          onClick={onClick}
+        >
+          Projects
+        </MobileHashLink>
+        <MobileRouterLink
+          title="Contact"
+          aria-label="Link to Contact page"
+          to="/contact"
+          onClick={onClick}
+        >
+          Contact
+        </MobileRouterLink>
+        <MobileIconLink
+          href="https://github.com/traczoskar"
+          target="_blank"
+          rel="noreferrer noopener"
+          aria-label="Link to GitHub profile"
+          title="Link to GitHub profile"
+          onClick={onClick}
+        >
+          <GitHubIcon />
+        </MobileIconLink>
+        <MobileIconLink
+          href="https://www.linkedin.com/in/traczoskar/"
+          target="_blank"
+          rel="noreferrer noopener"
+          aria-label="Link to LinkedIn profile"
+          title="Link to LinkedIn profile"
+          onClick={onClick}
+        >
+          <LinkedInIcon />
+        </MobileIconLink>
+        <ThemeSwitch width={40} height={40} />
+      </MobileNavLinks>
+    </>
+  );
+};
+
+export default MobileNavigation;
