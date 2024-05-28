b48e84c7cd311c87331366411b93526bf3f37be2
traczoskar <155188539+traczoskar@users.noreply.github.com>
2024-05-28 18:52:03 +0200

    refactor: Improve mobile menu feature in NavSection and enhance mobile responsiveness

--- src/common/NavSection/index.tsx
+++ src/common/NavSection/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { ThemeSwitch } from "../ThemeSwitch";
 import { ReactComponent as GitHubIcon } from "../../assets/icons/git_icon.svg";
 import { ReactComponent as LinkedInIcon } from "../../assets/icons/linkedIn_icon.svg";
@@ -14,93 +14,53 @@ import {
   NavWrapper,
   BurgerMenu,
   BurgerIcon,
-  MobileNavLinks,
   NavRouterLink,
 } from "./styled";
 import PageLogo from "../PageLogo";
-import { useTheme } from "styled-components";
+import MobileNavigation from "./MobileNavigation";
 
 const NavSection: React.FC = () => {
   const [isOpen, setIsOpen] = useState<boolean>(false);
-  // const theme = useTheme();
-  const isTabletOrMobile: boolean = useMediaQuery({
+  const isMobile: boolean = useMediaQuery({
     query: `(max-width: 767px)`,
   });
+  const isTabletVertical: boolean = useMediaQuery({
+    query: `(max-width: 991px)`,
+  });
 
-  const toggleMenu = () => {
+  const toggleMenu = (): void => {
     setIsOpen(!isOpen);
   };
+
+  useEffect(() => {
+    if (isOpen) {
+      document.body.style.overflow = "hidden";
+    } else {
+      document.body.style.overflow = "unset";
+    }
+  }, [isOpen]);
 
   return (
     <NavBar id="header">
       <NavWrapper>
         <NavHeaderLink to="/">
           <NavHeader>
-            <PageLogo
-              height={isTabletOrMobile ? 28 : 35}
-              width={isTabletOrMobile ? 28 : 35}
-            />
+            <PageLogo height={isMobile ? 28 : 35} width={isMobile ? 28 : 35} />
             traczoskar.dev
           </NavHeader>
         </NavHeaderLink>
-        {isTabletOrMobile ? (
+        {isMobile ? (
           <div>
             <BurgerMenu onClick={toggleMenu}>
               <BurgerIcon />
             </BurgerMenu>
             <motion.div
-              initial={{ height: 0 }}
-              animate={{ height: isOpen ? "auto" : 0 }}
+              initial={{ opacity: 0 }}
+              animate={{ opacity: isOpen ? 1 : 0 }}
               transition={{ duration: 0.3 }}
+              style={{ display: isOpen ? "block" : "none" }}
             >
-              <MobileNavLinks isOpen={isOpen}>
-                <NavRouterLink
-                  title="About Me"
-                  aria-label="Link to About Me page"
-                  to="/about"
-                  onClick={toggleMenu}
-                >
-                  About Me
-                </NavRouterLink>
-                <NavHashLink
-                  smooth
-                  to="/#projects"
-                  aria-label="Link to Projects section"
-                  title="Projects"
-                  onClick={toggleMenu}
-                >
-                  Projects
-                </NavHashLink>
-                <NavRouterLink
-                  title="Contact"
-                  aria-label="Link to Contact page"
-                  to="/contact"
-                  onClick={toggleMenu}
-                >
-                  Contact
-                </NavRouterLink>
-                <NavIconLink
-                  href="https://github.com/traczoskar"
-                  target="_blank"
-                  rel="noreferrer noopener"
-                  aria-label="Link to GitHub profile"
-                  title="Link to GitHub profile"
-                  onClick={toggleMenu}
-                >
-                  <GitHubIcon width={30} height={30} />
-                </NavIconLink>
-                <NavIconLink
-                  href="https://www.linkedin.com/in/traczoskar/"
-                  target="_blank"
-                  rel="noreferrer noopener"
-                  aria-label="Link to LinkedIn profile"
-                  title="Link to LinkedIn profile"
-                  onClick={toggleMenu}
-                >
-                  <LinkedInIcon width={30} height={30} />
-                </NavIconLink>
-                <ThemeSwitch />
-              </MobileNavLinks>
+              <MobileNavigation isOpen={isOpen} onClick={toggleMenu} />
             </motion.div>
           </div>
         ) : (
@@ -134,7 +94,10 @@ const NavSection: React.FC = () => {
               aria-label="Link to GitHub profile"
               title="Link to GitHub profile"
             >
-              <GitHubIcon width={30} height={30} />
+              <GitHubIcon
+                width={isTabletVertical ? 25 : 30}
+                height={isTabletVertical ? 25 : 30}
+              />
             </NavIconLink>
             <NavIconLink
               href="https://www.linkedin.com/in/traczoskar/"
@@ -143,7 +106,10 @@ const NavSection: React.FC = () => {
               aria-label="Link to LinkedIn profile"
               title="Link to LinkedIn profile"
             >
-              <LinkedInIcon width={30} height={30} />
+              <LinkedInIcon
+                width={isTabletVertical ? 25 : 30}
+                height={isTabletVertical ? 25 : 30}
+              />
             </NavIconLink>
             <ThemeSwitch />
           </NavLinks>
