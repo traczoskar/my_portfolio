647c530c16b92f8d29cab6245e0301df1e9d3514
traczoskar <155188539+traczoskar@users.noreply.github.com>
2024-05-28 17:33:56 +0200

    refactor: Update Header styling for responsive design

--- src/views/HomePage/Header/styled.ts
+++ src/views/HomePage/Header/styled.ts
@@ -7,21 +7,39 @@ export const SectionDividerPurposeOnly = styled.div`
 export const Section = styled.div`
   margin: 0 auto;
   max-width: 1200px;
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.tabletHorMax}) {
+    padding: 0 3rem;
+  }
+  @media (max-width: ${({ theme }) => theme.breakpoints.mobileMax}) {
+    padding: 0 1rem;
+  }
 `;
 
-export const HeaderWrapper = styled.header`
+export const HeaderSectionWrapper = styled.header`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: 8rem 0 15rem 0;
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.tabletHorMax}) {
+    margin: 4rem 0 9rem 0;
+  }
+  @media (max-width: ${({ theme }) => theme.breakpoints.mobileMax}) {
+    margin: 3rem 0 8rem 0;
+  }
+`;
+
+export const HeaderWrapper = styled.article`
   display: flex;
   justify-content: center;
   align-items: center;
   flex-direction: column;
   gap: 1rem;
-`;
 
-export const HeaderSectionWrapper = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  margin: 8rem 0 15rem 0;
+  @media (max-width: ${({ theme }) => theme.breakpoints.mobileMax}) {
+    gap: 0.75rem;
+  }
 `;
 
 export const HeaderGreeter = styled.h2`
@@ -31,6 +49,14 @@ export const HeaderGreeter = styled.h2`
   padding: 0;
   letter-spacing: 0.03rem;
   text-align: center;
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.tabletHorMax}) {
+    font-size: 3vw;
+  }
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.mobileMax}) {
+    font-size: 5vw;
+  }
 `;
 
 export const SpecialTextGreeter = styled.span`
@@ -42,6 +68,13 @@ export const HeaderContent = styled.h3`
   margin: 0;
   padding: 0;
   font-size: 3rem;
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.tabletHorMax}) {
+    font-size: 3.5vw;
+  }
+  @media (max-width: ${({ theme }) => theme.breakpoints.mobileMax}) {
+    font-size: 7vw;
+  }
 `;
 
 export const SpecialText = styled.span`
@@ -58,12 +91,26 @@ export const SubHeaderContent = styled.h4`
   font-size: 1.3rem;
   font-weight: 300;
   letter-spacing: 0.03rem;
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.tabletHorMax}) {
+    font-size: 1.7vw;
+  }
+  @media (max-width: ${({ theme }) => theme.breakpoints.mobileMax}) {
+    font-size: 3.75vw;
+  }
 `;
 
 export const CurlyBraces = styled.span`
   font-weight: 500;
   color: #777777;
   font-size: 3.5rem;
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.tabletHorMax}) {
+    font-size: 3.6vw;
+  }
+  @media (max-width: ${({ theme }) => theme.breakpoints.mobileMax}) {
+    font-size: 8vw;
+  }
 `;
 
 export const Paragraph = styled.p`
@@ -72,6 +119,15 @@ export const Paragraph = styled.p`
   font-weight: 400;
   letter-spacing: 0.03rem;
   line-height: 1.8;
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.tabletHorMax}) {
+    text-align: center;
+    font-size: 1.3vw;
+  }
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.mobileMax}) {
+    font-size: 3.3vw;
+  }
 `;
 
 export const ImageWrapper = styled.div`
@@ -84,6 +140,10 @@ export const ButtonWrapper = styled.div`
   display: flex;
   align-items: flex-start;
   width: 100%;
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.tabletHorMax}) {
+    justify-content: center;
+  }
 `;
 
 export const Image = styled.img`
@@ -96,5 +156,15 @@ export const Image = styled.img`
     filter: drop-shadow(0 10px 10px ${({ theme }) => theme.colors.basic});
     /* filter: drop-shadow(0 10px 10px #aaaaaa); */
     transform: translateY(-5px);
+  }
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.tabletHorMax}) {
+    width: 32vw;
+  }
+  @media (max-width: ${({ theme }) => theme.breakpoints.tabletVerticalMax}) {
+    width: 36vw;
+  }
+  @media (max-width: ${({ theme }) => theme.breakpoints.mobileMax}) {
+    width: 70vw;
   }
 `;
