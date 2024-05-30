f8ff2ac35a257eaad6be6a6e2e818636ddd6fe0e
traczoskar <155188539+traczoskar@users.noreply.github.com>
2024-05-30 15:00:51 +0200

    refactor: Add languageSlice to main Redux store file

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import themeReducer from "../slices/themeSlice";
+import languageReducer from "../slices/languageSlice";
 import projectDetailsReducer from "../slices/projectDetailsSlice";
 import selectedProjectReducer from "../slices/selectedProjectSlice";
 
 const store = configureStore({
   reducer: {
     theme: themeReducer,
+    language: languageReducer,
     projectDetails: projectDetailsReducer,
     selectedProject: selectedProjectReducer,
   },
