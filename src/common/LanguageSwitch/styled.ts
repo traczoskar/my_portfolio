f6446e68e0edc5e48efa4992217f8407732361a0
traczoskar <155188539+traczoskar@users.noreply.github.com>
2024-05-30 15:01:06 +0200

    feat: Add styled component for LanguageSwitch button

---
+++ src/common/LanguageSwitch/styled.ts
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const Button = styled.button`
+  display: flex;
+  background: none;
+  border: none;
+  color: inherit;
+  font-size: 1.5rem;
+  font-weight: 500;
+`;
