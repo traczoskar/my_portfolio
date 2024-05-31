6a647c7c1af85304c924d40b47817ca144d455e0
traczoskar <155188539+traczoskar@users.noreply.github.com>
2024-05-31 13:07:19 +0200

    refactor: Update ContactPage to support language switching

--- src/views/ContactPage/index.tsx
+++ src/views/ContactPage/index.tsx
@@ -17,10 +17,13 @@ import Footer from "../../common/Footer";
 import { useEffect, useState } from "react";
 import ModalMessage from "./ModalMessage";
 import { useTheme } from "styled-components";
+import { useSelector } from "react-redux";
+import { selectIsLanguageEN } from "../../slices/languageSlice";
 
 const ContactPage = () => {
   const [isCopied, setIsCopied] = useState<boolean>(false);
   const theme = useTheme();
+  const isLanguageEN: boolean = useSelector(selectIsLanguageEN);
 
   useEffect(() => {
     if (isCopied) {
@@ -36,13 +39,26 @@ const ContactPage = () => {
         <NavSection />
         <ContactPageWrapper>
           <ContactTitle>
-            Drop me <SpecialText>a line</SpecialText>, I'm just{" "}
-            <SpecialText>a click</SpecialText> away!
+            {isLanguageEN ? (
+              <>
+                Drop me <SpecialText>a line</SpecialText>, I'm just{" "}
+                <SpecialText>a click</SpecialText> away!
+              </>
+            ) : (
+              <>
+                Zostaw mi <SpecialText>wiadomość</SpecialText>, to tylko kilka{" "}
+                <SpecialText>kliknięć!</SpecialText>
+              </>
+            )}
           </ContactTitle>
           <ContactLinks>
             <ContactLink
               href={`mailto:${ABOUT.email}`}
-              title="E-mail to contact@traczoskar.dev"
+              title={
+                isLanguageEN
+                  ? "E-mail to contact@traczoskar.dev"
+                  : "E-mail do contact@traczoskar.dev"
+              }
             >
               <MailIcon width={30} height={30} />
               E-mail
@@ -50,7 +66,7 @@ const ContactPage = () => {
             <ContactLink
               href={ABOUT.socials[0].url}
               target="_blank"
-              title="GitHub profile"
+              title={isLanguageEN ? "GitHub profile" : "Profil na GitHubie"}
             >
               <GitHubIcon width={30} height={30} />
               GitHub
@@ -58,7 +74,7 @@ const ContactPage = () => {
             <ContactLink
               href={ABOUT.socials[1].url}
               target="_blank"
-              title="LinkedIn profile"
+              title={isLanguageEN ? "LinkedIn profile" : "Profil na LinkedIn"}
             >
               <LinkedInIcon width={30} height={30} />
               LinkedIn
@@ -67,7 +83,11 @@ const ContactPage = () => {
               text={ABOUT.email}
               onCopy={() => setIsCopied(true)}
             >
-              <ContactLink title="Copy e-mail address">
+              <ContactLink
+                title={
+                  isLanguageEN ? "Copy e-mail address" : "Skopiuj adres e-mail"
+                }
+              >
                 <CopyIcon width={30} height={30} />
                 contact@traczoskar.dev
               </ContactLink>
