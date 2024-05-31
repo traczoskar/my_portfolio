b4fecafc300d753e54285cfc0e95e5e3849449e3
traczoskar <155188539+traczoskar@users.noreply.github.com>
2024-05-31 12:46:57 +0200

    refactor: Update LanguageSwitch component to improve language selection UI

--- src/common/LanguageSwitch/styled.ts
+++ src/common/LanguageSwitch/styled.ts
@@ -2,9 +2,28 @@ import styled from "styled-components";
 
 export const Button = styled.button`
   display: flex;
+  align-items: center;
   background: none;
   border: none;
   color: inherit;
-  font-size: 1.5rem;
+  font-size: 1.2rem;
   font-weight: 500;
+  cursor: pointer;
+  transition: font-weight 0.3s, color 0.2s;
+
+  &:hover {
+    color: ${({ theme }) => theme.colors.basic};
+  }
+
+  span {
+    transition: all 0.2s;
+    font-weight: 400;
+    font-size: 1rem;
+  }
+
+  .active {
+    transition: all 0.2s;
+    font-weight: 800;
+    font-size: 1.1rem;
+  }
 `;
