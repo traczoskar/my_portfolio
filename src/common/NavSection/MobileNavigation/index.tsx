d06e7dd6b0bd739b37d910ef85a3adbd353efa52
traczoskar <155188539+traczoskar@users.noreply.github.com>
2024-06-01 15:57:35 +0200

    refactor: Update MobileNavigation component to improve accessibility and correct some styling. Also added page logo as a link to home page

--- src/common/NavSection/MobileNavigation/index.tsx
+++ src/common/NavSection/MobileNavigation/index.tsx
@@ -6,11 +6,15 @@ import {
   MobileHashLink,
   MobileIconLink,
   MobileNavLinks,
+  MobileNavLogo,
   MobileRouterLink,
 } from "./styled";
 import { ReactComponent as CloseIcon } from "../../../assets/icons/close.svg";
 import { ReactComponent as GitHubIcon } from "../../../assets/icons/git_icon.svg";
 import { ReactComponent as LinkedInIcon } from "../../../assets/icons/linkedIn_icon.svg";
+import { useSelector } from "react-redux";
+import { selectIsLanguageEN } from "../../../slices/languageSlice";
+import PageLogo from "../../PageLogo";
 
 interface MobileNavigationProps {
   isOpen: boolean;
@@ -21,19 +25,23 @@ const MobileNavigation: React.FC<MobileNavigationProps> = ({
   isOpen,
   onClick,
 }) => {
+  const isLanguageEN: boolean = useSelector(selectIsLanguageEN);
   return (
     <>
       <MobileNavLinks isOpen={isOpen}>
         <CloseButton onClick={onClick}>
-          <CloseIcon width={35} height={35} />
+          <CloseIcon width={25} height={25} />
         </CloseButton>
+        <MobileNavLogo onClick={onClick} to="/">
+          <PageLogo height={26} width={26} />
+        </MobileNavLogo>
         <MobileRouterLink
           title="About Me"
           aria-label="Link to About Me page"
           to="/about"
           onClick={onClick}
         >
-          About Me
+          {isLanguageEN ? "About Me" : "O mnie"}
         </MobileRouterLink>
         <MobileHashLink
           smooth
@@ -42,7 +50,7 @@ const MobileNavigation: React.FC<MobileNavigationProps> = ({
           title="Projects"
           onClick={onClick}
         >
-          Projects
+          {isLanguageEN ? "Projects" : "Projekty"}
         </MobileHashLink>
         <MobileRouterLink
           title="Contact"
@@ -50,9 +58,9 @@ const MobileNavigation: React.FC<MobileNavigationProps> = ({
           to="/contact"
           onClick={onClick}
         >
-          Contact
+          {isLanguageEN ? "Contact" : "Kontakt"}
         </MobileRouterLink>
-        <LanguageSwitch onClick={onClick} />
+        <LanguageSwitch />
         <IconsWrapper>
           <MobileIconLink
             href="https://github.com/traczoskar"
@@ -62,7 +70,7 @@ const MobileNavigation: React.FC<MobileNavigationProps> = ({
             title="Link to GitHub profile"
             onClick={onClick}
           >
-            <GitHubIcon width={35} height={35} />
+            <GitHubIcon width={30} height={30} />
           </MobileIconLink>
           <MobileIconLink
             href="https://www.linkedin.com/in/traczoskar/"
@@ -72,9 +80,9 @@ const MobileNavigation: React.FC<MobileNavigationProps> = ({
             title="Link to LinkedIn profile"
             onClick={onClick}
           >
-            <LinkedInIcon width={35} height={35} />
+            <LinkedInIcon width={30} height={30} />
           </MobileIconLink>
-          <ThemeSwitch width={35} height={35} padding={"0"} />
+          <ThemeSwitch width={30} height={30} padding={"0"} />
         </IconsWrapper>
       </MobileNavLinks>
     </>
