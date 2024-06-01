3dcb9128c602e82787a03cbde4b39feb2cd9d401
traczoskar <155188539+traczoskar@users.noreply.github.com>
2024-06-01 09:16:47 +0200

    refactor: Update LanguageSwitch component to include onClick prop for custom click event handling

--- src/common/LanguageSwitch/index.tsx
+++ src/common/LanguageSwitch/index.tsx
@@ -2,7 +2,11 @@ import { useDispatch, useSelector } from "react-redux";
 import { Button } from "./styled";
 import { selectIsLanguageEN, toggleLanguage } from "../../slices/languageSlice";
 
-const LanguageSwitch = () => {
+interface LanguageSwitchProps {
+  onClick?: () => void;
+}
+
+const LanguageSwitch: React.FC<LanguageSwitchProps> = ({ onClick }) => {
   const isLanguageEN = useSelector(selectIsLanguageEN);
   const dispatch = useDispatch();
 
@@ -11,7 +15,12 @@ const LanguageSwitch = () => {
       title={
         isLanguageEN ? "Change language to Polish" : "Zmień język na angielski"
       }
-      onClick={() => dispatch(toggleLanguage())}
+      onClick={() => {
+        dispatch(toggleLanguage());
+        if (onClick) {
+          onClick();
+        }
+      }}
     >
       <span className={isLanguageEN ? "active" : ""}>EN&nbsp;</span> |{" "}
       <span className={!isLanguageEN ? "active" : ""}>&nbsp;PL</span>
