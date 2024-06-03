e3a9f674bab8cef44920409e7d3ac5a74dd5d2eb
traczoskar <155188539+traczoskar@users.noreply.github.com>
2024-06-03 15:42:52 +0200

    refactor: Improve responsiveness of AboutMe component and update padding for mobile devices

--- src/views/AboutMe/Education/styled.ts
+++ src/views/AboutMe/Education/styled.ts
@@ -7,10 +7,18 @@ interface EducationItemProps {
 export const EducationContainer = styled.div`
   display: flex;
   flex-direction: column;
+  width: 100%;
   align-items: center;
   max-width: 1200px;
   margin: 3rem 0 8rem 0;
   gap: 3rem;
+  @media (max-width: ${({ theme }) => theme.breakpoints.tabletHorMax}) {
+    padding: 0 3rem;
+  }
+  @media (max-width: ${({ theme }) => theme.breakpoints.mobileMax}) {
+    margin: 2rem 0 3rem 0;
+    padding: 0 5vw;
+  }
 `;
 
 export const Title = styled.h3`
@@ -18,6 +26,11 @@ export const Title = styled.h3`
   margin: 0;
   font-weight: 800;
   font-size: 3rem;
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.mobileMax}) {
+    align-self: center;
+    font-size: 8vw;
+  }
 `;
 
 export const EducationList = styled.ul`
@@ -38,6 +51,9 @@ export const EducationItem = styled.li<EducationItemProps>`
   border: 1px solid #ddd;
   border-radius: 0.5rem;
   background: ${({ background }) => background};
+  @media (max-width: ${({ theme }) => theme.breakpoints.mobileMax}) {
+    padding: 1.25rem;
+  }
 `;
 
 export const CourseOverview = styled.div`
@@ -50,12 +66,18 @@ export const CourseTitle = styled.h4`
   font-weight: 800;
   font-size: 1.5rem;
   margin: 0;
+  @media (max-width: ${({ theme }) => theme.breakpoints.mobileMax}) {
+    font-size: 5vw;
+  }
 `;
 
 export const EducationName = styled.p`
   font-weight: 500;
   margin: 0;
   font-size: 1.3rem;
+  @media (max-width: ${({ theme }) => theme.breakpoints.mobileMax}) {
+    font-size: 4vw;
+  }
 `;
 
 export const CourseDescription = styled.p`
@@ -63,6 +85,9 @@ export const CourseDescription = styled.p`
   font-size: 1.1rem;
   line-height: 1.5;
   text-align: justify;
+  @media (max-width: ${({ theme }) => theme.breakpoints.mobileMax}) {
+    font-size: 3.5vw;
+  }
 `;
 
 export const CourseInfo = styled.div`
@@ -74,12 +99,20 @@ export const CourseAdditional = styled.p`
   margin: 0;
   font-size: 0.9rem;
   color: ${({ theme }) => theme.colors.aboutMePage.education.additionalText};
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.mobileMax}) {
+    font-size: 3.5vw;
+  }
 `;
 
 export const GridDivider = styled.div`
   display: grid;
   grid-template-columns: 2fr 1fr;
   gap: 2rem;
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.mobileMax}) {
+    grid-template-columns: 1fr;
+  }
 `;
 
 export const CertificateContainer = styled.div`
@@ -94,6 +127,9 @@ export const CertificateInstructions = styled.p`
   margin: 1rem 0 0 0;
   font-size: 0.8rem;
   color: ${({ theme }) => theme.colors.aboutMePage.education.additionalText};
+  @media (max-width: ${({ theme }) => theme.breakpoints.mobileMax}) {
+    font-size: 0.7rem;
+  }
 `;
 
 export const Certificate = styled.img`
@@ -111,5 +147,8 @@ export const Certificate = styled.img`
     cursor: zoom-in;
     outline: 3px solid ${({ theme }) => theme.colors.basic};
     transform: scale(1.05);
+  }
+  @media (max-width: ${({ theme }) => theme.breakpoints.mobileMax}) {
+    height: 45vw;
   }
 `;
