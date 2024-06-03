4e291a4505af6ea7390bf224d8c053776c724c72
traczoskar <155188539+traczoskar@users.noreply.github.com>
2024-06-03 15:43:53 +0200

    refactor: added motion fade in animation for Header component

--- src/views/HomePage/Header/index.tsx
+++ src/views/HomePage/Header/index.tsx
@@ -39,9 +39,20 @@ const Header: React.FC<HeaderProps> = ({
   const isLanguageEN: boolean = useSelector(selectIsLanguageEN);
   const theme = useTheme();
 
+  const contentVariants = {
+    hidden: { opacity: 0 },
+    visible: {
+      opacity: 1,
+      transition: {
+        delay: 0.5,
+        duration: 1.0,
+      },
+    },
+  };
+
   return (
     <SectionDividerPurposeOnly>
-      <Section>
+      <Section initial="hidden" animate="visible" variants={contentVariants}>
         <HeaderSectionWrapper>
           <GridSection>
             <HeaderWrapper>
