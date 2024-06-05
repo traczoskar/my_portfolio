3da327fe436461af0d58fe0b5605045454846a64
traczoskar <155188539+traczoskar@users.noreply.github.com>
2024-06-05 17:39:17 +0200

    refactor: Add lazy loading for portfolio images

--- src/views/HomePage/Portfolio/ProjectDetails/ScreenshotGallery/index.tsx
+++ src/views/HomePage/Portfolio/ProjectDetails/ScreenshotGallery/index.tsx
@@ -1,7 +1,8 @@
-import { useState } from "react";
-import { GalleryContainer, Thumbnail } from "./styled";
+import { useEffect, useState } from "react";
+import { GalleryContainer, Placeholder, Thumbnail } from "./styled";
 import ImageViewer from "../../../../../common/ImageViewer";
 import { Screenshot } from "../../../../../types/types";
+import Loader from "../../../../../common/Loader";
 
 interface ScreenshotGalleryProps {
   screenshots: Screenshot[] | undefined;
@@ -17,6 +18,21 @@ const ScreenshotGallery = ({
   const [selectedImageIndex, setSelectedImageIndex] = useState<number | null>(
     null
   );
+  const [loadedImages, setLoadedImages] = useState<boolean[]>([]);
+
+  useEffect(() => {
+    if (screenshots) {
+      setLoadedImages(new Array(screenshots.length).fill(false));
+    }
+  }, [screenshots]);
+
+  const handleImageLoad = (index: number) => {
+    setLoadedImages((prev) => {
+      const newLoadedImages = [...prev];
+      newLoadedImages[index] = true;
+      return newLoadedImages;
+    });
+  };
 
   const handleImageClick = (index: number) => {
     setSelectedImageIndex(index);
@@ -32,12 +48,21 @@ const ScreenshotGallery = ({
     <>
       <GalleryContainer>
         {screenshots?.map((screenshot, index) => (
-          <Thumbnail
-            key={index}
-            src={screenshot.imageUrl}
-            alt={screenshot.alt}
-            onClick={() => handleImageClick(index)}
-          />
+          <div key={index}>
+            {!loadedImages[index] && (
+              <Placeholder>
+                <Loader />
+              </Placeholder>
+            )}
+            <Thumbnail
+              key={index}
+              src={screenshot.imageUrl}
+              alt={screenshot.alt}
+              onClick={() => handleImageClick(index)}
+              onLoad={() => handleImageLoad(index)}
+              style={{ display: loadedImages[index] ? "block" : "none" }}
+            />
+          </div>
         ))}
       </GalleryContainer>
       {selectedImageIndex !== null && (
