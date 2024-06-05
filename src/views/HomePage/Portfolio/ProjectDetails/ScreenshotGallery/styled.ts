805eb33237ff99680e008c48df989498519795dd
traczoskar <155188539+traczoskar@users.noreply.github.com>
2024-06-05 17:39:38 +0200

    refactor: Add Placeholder component to handle lazy loading

--- src/views/HomePage/Portfolio/ProjectDetails/ScreenshotGallery/styled.ts
+++ src/views/HomePage/Portfolio/ProjectDetails/ScreenshotGallery/styled.ts
@@ -24,3 +24,16 @@ export const Thumbnail = styled.img`
       ${({ theme }) => theme.colors.projectDetails.screenshotOutline};
   }
 `;
+
+export const Placeholder = styled.div`
+  width: 100%;
+  height: 9.5vw;
+  border-radius: 0.5rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: #888888;
+  box-shadow: 0 1px 1px hsl(0deg 0% 0% / 0.075),
+    0 2px 2px hsl(0deg 0% 0% / 0.075), 0 4px 4px hsl(0deg 0% 0% / 0.075),
+    0 8px 8px hsl(0deg 0% 0% / 0.075), 0 16px 16px hsl(0deg 0% 0% / 0.075);
+`;
