5883a9e0afbc6867edcc8cb73327e147a41109c3
traczoskar <155188539+traczoskar@users.noreply.github.com>
2024-06-22 16:47:50 +0200

    refactor: Update Polish translation for portfolio description

--- src/views/HomePage/Portfolio/PortfolioDescription/index.tsx
+++ src/views/HomePage/Portfolio/PortfolioDescription/index.tsx
@@ -17,7 +17,7 @@ const PortfolioDescription: React.FC = () => {
       <PortfolioDescriptionContent>
         {isLanguageEN
           ? "Here's a curated collection of my recent projects. Click on any project to delve into more details about the technologies utilized and the skills acquired through their development journey. Take your time to browse and learn more about my work!"
-          : "Oto wyselekcjonowana kolekcja moich ostatnich projektów. Kliknij w dowolny projekt, aby dowiedzieć się więcej o wykorzystanych technologiach i umiejętnościach zdobytych podczas ich tworzenia. Przejrzyj i dowiedz się więcej o mojej pracy!"}
+          : "Oto wyselekcjonowana kolekcja moich ostatnich projektów. Kliknij na dowolny projekt, aby dowiedzieć się więcej o wykorzystanych technologiach i umiejętnościach zdobytych podczas ich tworzenia. Przejrzyj szczegóły i dowiedz się więcej o mojej pracy!"}
       </PortfolioDescriptionContent>
       <ClickHere />
     </DescriptionWrapper>
