07f4c3eaf1268dff3445b5c31bad628ad99eb6f8
traczoskar <155188539+traczoskar@users.noreply.github.com>
2024-06-23 22:06:51 +0200

    refactor: Update Modal component to include responsive behavior for tablet and mobile views

--- src/common/Modal/styled.ts
+++ src/common/Modal/styled.ts
@@ -1,5 +1,10 @@
 import styled from "styled-components";
 
+interface ThemeSwitchModalContainerProps {
+  $top?: string | false;
+  $right?: string | false;
+}
+
 export const ModalOverlay = styled.div`
   position: fixed;
   top: 0;
@@ -14,35 +19,48 @@ export const ModalOverlay = styled.div`
   backdrop-filter: blur(5px);
 `;
 
-export const ModalContent = styled.div`
+export const ModalContent = styled.section`
   background: ${({ theme }) => theme.colors.projectDetails.background};
   border-radius: 5px;
   box-shadow: 0 0 10px rgba(0, 0, 0, 0.5);
   max-width: 80%;
   max-height: 80%;
   overflow: auto;
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.tabletHorMax}) {
+    max-width: 100%;
+    max-height: 100%;
+    position: relative;
+  }
 `;
 
 export const CloseButton = styled.button`
   position: absolute;
   width: 2.5rem;
   height: 2.5rem;
-  top: 1rem;
-  right: 1rem;
+  top: 2rem;
+  right: 2rem;
   font-size: 1rem;
   background-color: #dddddd;
   border: none;
   border-radius: 0.5rem;
   cursor: pointer;
   transition: 0.2s ease;
+  z-index: 50;
 
   &:hover {
     background-color: #bbbbbb;
   }
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.tabletHorMax}) {
+    top: 1rem;
+    right: 1rem;
+  }
 `;
 
-export const ThemeSwitchModalContainer = styled.div`
+export const ThemeSwitchModalContainer = styled.div<ThemeSwitchModalContainerProps>`
+  z-index: 50;
   position: absolute;
-  top: 4.5rem;
-  right: 0.25rem;
+  top: ${({ $top }) => $top || "5rem"};
+  right: ${({ $right }) => $right || "1.25rem"};
 `;
