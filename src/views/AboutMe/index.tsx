384ffa47fd53bfd4266f130e330ad05c48ef58b6
traczoskar <155188539+traczoskar@users.noreply.github.com>
2024-07-02 13:34:52 +0200

    refactor: Improve responsiveness of AboutMe component

--- src/views/AboutMe/index.tsx
+++ src/views/AboutMe/index.tsx
@@ -44,8 +44,8 @@ const AboutMe: React.FC = () => {
   const theme = useTheme();
 
   const fadeInVariants = {
-    hidden: { opacity: 0, y: 20 },
-    visible: { opacity: 1, y: 0, transition: { duration: 0.5 } },
+    hidden: { opacity: 0 },
+    visible: { opacity: 1, transition: { duration: 0.5 } },
   };
 
   return (
