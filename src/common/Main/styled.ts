bee2c3768771818cbf9097c62218b0c7e3707594
traczoskar <155188539+traczoskar@users.noreply.github.com>
2024-07-02 18:30:03 +0200

    refactor: Update MainContainer background prop name to $backgroundColor

--- src/common/Main/styled.ts
+++ src/common/Main/styled.ts
@@ -2,12 +2,12 @@ import styled from "styled-components";
 import { DefaultTheme } from "styled-components";
 
 interface MainContainerProps {
-  backgroundColor?: ((theme: DefaultTheme) => string) | string;
+  $backgroundColor?: ((theme: DefaultTheme) => string) | string;
 }
 
 export const MainContainer = styled.main<MainContainerProps>`
   display: flex;
   flex-direction: column;
   flex-grow: 1;
-  background: ${({ backgroundColor }) => backgroundColor || "inherit"};
+  background: ${({ $backgroundColor }) => $backgroundColor || "inherit"};
 `;
