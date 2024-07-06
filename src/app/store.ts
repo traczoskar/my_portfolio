9e6446464d267c356a8591a5fd81cce1e525a395
traczoskar <155188539+traczoskar@users.noreply.github.com>
2024-07-06 16:13:03 +0200

    refactor: Add saga middleware to store configuration

--- src/app/store.ts
+++ src/app/store.ts
@@ -3,6 +3,10 @@ import themeReducer from "../slices/themeSlice";
 import languageReducer from "../slices/languageSlice";
 import projectDetailsReducer from "../slices/projectDetailsSlice";
 import selectedProjectReducer from "../slices/selectedProjectSlice";
+import createSagaMiddleware, { SagaMiddleware } from "redux-saga";
+import rootSaga from "./rootSaga";
+
+const sagaMiddleware: SagaMiddleware<object> = createSagaMiddleware();
 
 const store = configureStore({
   reducer: {
@@ -11,6 +15,10 @@ const store = configureStore({
     projectDetails: projectDetailsReducer,
     selectedProject: selectedProjectReducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(sagaMiddleware),
 });
+
+sagaMiddleware.run(rootSaga);
 
 export default store;
