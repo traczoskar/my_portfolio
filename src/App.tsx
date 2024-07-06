183aa55dc66df4c8ad6d06634f03056bfa9a22ea
traczoskar <155188539+traczoskar@users.noreply.github.com>
2024-07-07 00:10:14 +0200

    refactor: applied dynamic html language tag changing based on currently state of language Redux slice

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,8 @@
-import { BrowserRouter as Router } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  useLocation,
+  useParams,
+} from "react-router-dom";
 import { ThemeProvider } from "styled-components";
 import { GlobalStyle } from "./app/GlobalStyle";
 import { themeDark, themeLight } from "./app/theme";
@@ -6,12 +10,18 @@ import { useSelector } from "react-redux";
 import { selectIsDarkTheme } from "./slices/themeSlice";
 import AnimatedRoutes from "./app/AnimatedRoutes/AnimatedRoutes";
 import ScrollToTopButton from "./common/ScrollToTopButton";
+import { Helmet } from "react-helmet-async";
+import { selectIsLanguageEN } from "./slices/languageSlice";
 
 const App = () => {
-  const isDarkTheme = useSelector(selectIsDarkTheme);
+  const isDarkTheme: boolean = useSelector(selectIsDarkTheme);
+  const isLanguageEN: boolean = useSelector(selectIsLanguageEN);
 
   return (
     <ThemeProvider theme={isDarkTheme ? themeDark : themeLight}>
+      <Helmet>
+        <html lang={isLanguageEN ? "en" : "pl"} />
+      </Helmet>
       <GlobalStyle />
       <ScrollToTopButton />
       <Router>
