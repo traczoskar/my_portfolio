c27e9f3428bfba562daa851a48851ecb559e87bd
traczoskar <155188539+traczoskar@users.noreply.github.com>
2024-07-07 00:09:22 +0200

    refactor: Add react-helmet-async for better control over document head

--- src/index.tsx
+++ src/index.tsx
@@ -4,14 +4,17 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals.js";
 import store from "./app/store";
 import { Provider } from "react-redux";
+import { HelmetProvider } from "react-helmet-async";
 
 const root = ReactDOM.createRoot(document.getElementById("root")!);
 
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <HelmetProvider>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </HelmetProvider>
   </React.StrictMode>
 );
 
